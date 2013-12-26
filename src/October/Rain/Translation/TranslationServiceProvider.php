2687ca6ef9fa84bc6b02f51d0a3f8003f0e874a2
Sam Georges <sam@daftspunk.com>
2013-12-26 22:09:34 +1100

    Adds customised translator

---
+++ src/October/Rain/Translation/TranslationServiceProvider.php
@@ -0,0 +1,48 @@
+<?php namespace October\Rain\Translation;
+
+use Illuminate\Translation\FileLoader;
+use Illuminate\Support\ServiceProvider;
+
+class TranslationServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerLoader();
+
+        $this->app->bindShared('translator', function($app){
+            return new Translator($app['translation.loader'], $app['config']['app.locale'], $app['files']);
+        });
+    }
+
+    /**
+     * Register the translation line loader.
+     * @return void
+     */
+    protected function registerLoader()
+    {
+        $this->app->bindShared('translation.loader', function($app){
+            return new FileLoader($app['files'], $app['path'].'/lang');
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     * @return array
+     */
+    public function provides()
+    {
+        return array('translator', 'translation.loader');
+    }
+
+}
---
+++ src/October/Rain/Translation/Translator.php
@@ -0,0 +1,71 @@
+<?php namespace October\Rain\Translation;
+
+use Illuminate\Translation\Translator as IlluminateTranslator;
+use Illuminate\Translation\LoaderInterface;
+use Illuminate\Translation\FileLoader;
+use Illuminate\Filesystem\Filesystem;
+
+/**
+ * October translator class.
+ *
+ * @package translation
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class Translator extends IlluminateTranslator
+{
+    protected $appTranslator;
+
+    /**
+     * Create a new translator instance.
+     * @param  \Illuminate\Translation\LoaderInterface  $loader
+     * @param  string  $locale
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @return void
+     */
+    public function __construct(LoaderInterface $loader, $locale, Filesystem $files)
+    {
+        parent::__construct($loader, $locale);
+
+        $appLoader = new FileLoader($files, app_path().'/lang');
+        $this->appTranslator = new IlluminateTranslator($appLoader, $locale);
+    }
+
+    /**
+     * Add a new namespace to the loader.
+     * @param  string  $namespace
+     * @param  string  $hint
+     * @return void
+     */
+    public function addNamespace($namespace, $hint)
+    {
+        parent::addNamespace($namespace, $hint);
+
+        $namespaceAppPath = app_path().'/lang/'.str_replace('.', '/', $namespace);
+        $this->appTranslator->addNamespace($namespace, $namespaceAppPath);
+    }
+
+    /**
+     * Get the translation for the given key.
+     * @param  string  $key
+     * @param  array   $replace
+     * @param  string  $locale
+     * @return string
+     */
+    
+    public function get($key, array $replace = array(), $locale = null)
+    {
+        $nativeKey = $key;
+
+        if (starts_with($nativeKey, 'validation.') && !starts_with($nativeKey, 'validation.custom.') && !starts_with($nativeKey, 'validation.attributes.'))
+            $nativeKey = 'system::'.$key;
+
+        $value = parent::get($nativeKey, $replace, $locale);
+        if (strpos($nativeKey, '::') === false)
+            return $value;
+
+        $appValue = $this->appTranslator->get($key, $replace, $locale);
+
+        return $appValue == $key ? $value : $appValue;
+    }
+
+}
