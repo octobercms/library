276bb96085fad40973111352d38d563246c444a3
Sam Georges <sam@daftspunk.com>
2013-12-28 09:24:18 +1100

    Add minor helper classes (Num, Util, Yaml)

---
+++ src/October/Rain/Support/Number.php
@@ -0,0 +1,16 @@
+<?php namespace October\Rain\Support;
+
+/**
+ * Numeric helper
+ */
+class Number 
+{
+    /**
+     * Generate a random number based on time 
+     * @return int Random integer
+     */
+    public static function rand()
+    {
+        return str_replace('.', '', microtime(true));
+    }
+}
---
+++ src/October/Rain/Support/Util.php
@@ -0,0 +1,25 @@
+<?php namespace October\Rain\Support;
+
+/**
+ * General Utility helper
+ */
+class Util
+{
+
+    /**
+     * Extension of PHPs array_merge() except supporting 
+     * an array of arrays to merge.
+     *
+     * Usage:
+     *   Util::arrayMerge([$arr1, $arr2, $arr3]);
+     *   Util::arrayMerge($arr1, $arr2, $arr3);
+     */
+    public static function arrayMerge($collection)
+    {
+        if (func_num_args() > 1)
+            $collection = func_get_args();
+        
+        return call_user_func_array('array_merge', $collection);
+    }
+
+}
