f28a63793ce00ea5936376cac5ce10460912c02a
Sam Georges <sam@daftspunk.com>
2013-12-28 09:19:18 +1100

    Move App/Sys Exceptions to Rain lib

---
+++ src/October/Rain/Exceptions/ApplicationException.php
@@ -0,0 +1,12 @@
+<?php namespace Modules\System\Classes;
+
+/**
+ * This class represents an application exception.
+ * Application exceptions are not logged in the error log.
+ *
+ * @package system
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class ApplicationException extends ExceptionBase
+{
+}
---
+++ src/October/Rain/Exceptions/SystemException.php
@@ -0,0 +1,12 @@
+<?php namespace Modules\System\Classes;
+
+/**
+ * This class represents a critical system exception.
+ * System exceptions are logged in the error log.
+ *
+ * @package system
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class SystemException extends ExceptionBase
+{
+}
