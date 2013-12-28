08fc19495331f3e3cfcf9cc8e750f48e8e039709
Sam Georges <sam@daftspunk.com>
2013-12-28 11:57:51 +1100

    Fixes flaw in Laravel Seeder

--- src/October/Rain/Database/Updates/Seeder.php
+++ src/October/Rain/Database/Updates/Seeder.php
@@ -4,4 +4,20 @@ use Illuminate\Database\Seeder as SeederBase;
 
 class Seeder extends SeederBase
 {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function call($class)
+    {
+        $this->resolve($class)->run();
+
+        // @todo This has been sent as a PR to Laravel:
+        //       https://github.com/laravel/framework/pull/3064
+        //       Remove this entire method if it is accepted
+        if (isset($this->command))
+        {
+                $this->command->getOutput()->writeln("<info>Seeded:</info> $class");
+        }
+    }
 }
