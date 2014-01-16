64a8aa025d13e480154a3e94a541df7de65c69c6
Sam Georges <sam@daftspunk.com>
2014-01-16 18:08:08 +1100

    Move the Module Service provider to Rain\Support

---
+++ src/Support/ModuleServiceProvider.php
@@ -0,0 +1,74 @@
+<?php namespace October\Rain\Support;
+
+use October\Rain\Support\Facades\File;
+use Illuminate\Support\ServiceProvider as ServiceProviderBase;
+
+abstract class ModuleServiceProvider extends ServiceProviderBase
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     * @return void
+     */
+    public function boot()
+    {
+        if ($module = $this->getModule(func_get_args())) {
+            /*
+             * Register paths for: config, translator, view
+             */
+            $this->package($module, $module, base_path() . '/modules/' . $module);
+        }
+    }
+
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+        if ($module = $this->getModule(func_get_args())) {
+            /*
+             * Add routes, if available
+             */
+            $routesFile = base_path() . '/modules/' . $module . '/routes.php';
+            if (File::exists($routesFile))
+                require $routesFile;
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+    public function getModule($args)
+    {
+        $module = (isset($args[0]) and is_string($args[0])) ? $args[0] : null;
+        return $module;
+    }
+
+    /**
+     * Registers a new console (artisan) command
+     * @param $key The command name
+     * @param $class The command class
+     * @return void
+     */
+    public function registerConsoleCommand($key, $class)
+    {
+        $key = 'command.'.$key;
+        $this->app[$key] = $this->app->share(function($app) use ($class) {
+            return new $class;
+        });
+
+        $this->commands($key);
+    }
+}
