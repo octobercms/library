cea66c88ee304afffd10f79e20df49d50281f036
Sam Georges <sam@daftspunk.com>
2014-02-02 01:34:48 +1100

    K.I.S.S

--- src/Html/HtmlBuilder.php
+++ src/Html/HtmlBuilder.php
@@ -27,15 +27,8 @@ class HtmlBuilder extends HtmlBuilderBase
         if (is_null($value))
             return;
 
-        if (is_array($value)) {
-            // Encode the json to reside inside HTML attribute value
-            $value = htmlspecialchars(json_encode($value), ENT_QUOTES, 'UTF-8');
-
-            // Remove the wrapped curly brackets
-            $value = substr($value, 1, -1);
-
-            return $key."='".$value."'";
-        }
+        if (is_array($value))
+            $value = substr(htmlspecialchars(json_encode($value), ENT_QUOTES, 'UTF-8'), 1, -1);
 
         return $key.'="'.e($value).'"';
     }
