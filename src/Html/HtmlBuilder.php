3e4e8c96b22ce185497c55d9b19241187ed7b188
Sam Georges <sam@daftspunk.com>
2014-02-02 01:28:28 +1100

    Add notes, break off mustaches { }. Tested +OK

--- src/Html/HtmlBuilder.php
+++ src/Html/HtmlBuilder.php
@@ -27,8 +27,15 @@ class HtmlBuilder extends HtmlBuilderBase
         if (is_null($value))
             return;
 
-        if (is_array($value))
-            return $key."='".htmlspecialchars(json_encode($value), ENT_QUOTES, 'UTF-8')."'";
+        if (is_array($value)) {
+            // Encode the json to reside inside HTML attribute value
+            $value = htmlspecialchars(json_encode($value), ENT_QUOTES, 'UTF-8');
+
+            // Remove the wrapped curly brackets
+            $value = substr($value, 1, -1);
+
+            return $key."='".$value."'";
+        }
 
         return $key.'="'.e($value).'"';
     }
