14aaad4884f0931eb95bcb595b925ce450404d64
Sam Georges <sam@daftspunk.com>
2014-02-01 12:22:49 +1100

    Add 'field' column population and constraints, models now support multiple attachment definitions

--- src/Database/Relations/AttachOneOrMany.php
+++ src/Database/Relations/AttachOneOrMany.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Db;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 trait AttachOneOrMany
 {
@@ -22,12 +23,54 @@ trait AttachOneOrMany
     }
 
     /**
+     * Set the field (relation name) constraint on the query.
+     * @return void
+     */
+    public function addConstraints()
+    {
+        if (static::$constraints) {
+            parent::addConstraints();
+
+            $this->query->where('field', $this->relationName);
+        }
+    }
+
+    /**
+     * Add the field constraint for a relationship count query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  \Illuminate\Database\Eloquent\Builder  $parent
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function getRelationCountQuery(Builder $query, Builder $parent)
+    {
+        $query = parent::getRelationCountQuery($query, $parent);
+
+        return $query->where('field', $this->relationName);
+    }
+
+    /**
+     * Set the field constraint for an eager load of the relation.
+     *
+     * @param  array  $models
+     * @return void
+     */
+    public function addEagerConstraints(array $models)
+    {
+        parent::addEagerConstraints($models);
+
+        $this->query->where('field', $this->relationName);
+    }
+
+    /**
      * Save the supplied related model.
      */
     public function save(Model $model, $sessionKey = null)
     {
         if (!array_key_exists('public', $model->attributes))
-            $model->public = $this->isPublic();
+            $model->setAttribute('public', $this->isPublic());
+
+        $model->setAttribute('field', $this->relationName);
 
         return $this->saveDefer($model, $sessionKey);
     }
@@ -39,6 +82,8 @@ trait AttachOneOrMany
     {
         if (!array_key_exists('public', $attributes))
             $attributes = ['public' => $this->isPublic()] + $attributes;
+
+        $attributes['field'] = $this->relationName;
 
         return $this->createDefer($attributes, $sessionKey);
     }
@@ -54,6 +99,7 @@ trait AttachOneOrMany
         if ($sessionKey === null) {
             $model->setAttribute($this->getPlainForeignKey(), $this->parent->getKey());
             $model->setAttribute($this->getPlainMorphType(), $this->morphClass);
+            $model->setAttribute('field', $this->relationName);
         }
         else {
             $this->parent->bindDeferred($this->relationName, $model, $sessionKey);
@@ -70,6 +116,7 @@ trait AttachOneOrMany
             // Make this model an orphan ;~(
             $model->setAttribute($this->getPlainForeignKey(), null);
             $model->setAttribute($this->getPlainMorphType(), null);
+            $model->setAttribute('field', null);
         }
         else {
             $this->parent->unbindDeferred($this->relationName, $model, $sessionKey);
