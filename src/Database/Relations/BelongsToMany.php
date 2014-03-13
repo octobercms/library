52819de428abea977fb140561965e4573e60a403
alekseybobkov <aleksey.bobkov@gmail.com>
2014-03-14 08:39:32 +1100

    Added a required namespace

--- src/Database/Relations/BelongsToMany.php
+++ src/Database/Relations/BelongsToMany.php
@@ -1,6 +1,7 @@
 <?php namespace October\Rain\Database\Relations;
 
 use Illuminate\Database\Eloquent\Relations\BelongsToMany as BelongsToManyBase;
+use Illuminate\Database\Eloquent\Model;
 
 class BelongsToMany extends BelongsToManyBase
 {
