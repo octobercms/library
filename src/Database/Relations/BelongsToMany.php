fcb492a25eae58a170066b0d85f1dbc84d2d5a03
Sam Georges <sam@daftspunk.com>
2014-03-17 20:49:58 +1100

    Minor naming fix

--- src/Database/Relations/BelongsToMany.php
+++ src/Database/Relations/BelongsToMany.php
@@ -8,10 +8,10 @@ class BelongsToMany extends BelongsToManyBase
     /**
      * Adds a model to this relationship type.
      */
-    public function add(Model $model, $sessionKey = null, $joining = []) 
+    public function add(Model $model, $sessionKey = null, $pivotData = [])
     {
         if ($sessionKey === null) {
-            $this->attach($model->getKey(), $joining);
+            $this->attach($model->getKey(), $pivotData);
         }
         else {
             $this->parent->bindDeferred($this->relationName, $model, $sessionKey);
@@ -21,7 +21,7 @@ class BelongsToMany extends BelongsToManyBase
     /**
      * Removes a model from this relationship type.
      */
-    public function remove(Model $model, $sessionKey = null) 
+    public function remove(Model $model, $sessionKey = null)
     {
         if ($sessionKey === null) {
             $this->detach($model->getKey());
