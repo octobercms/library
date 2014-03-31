0862ab6502f78de1fd245803fa1657ff43bde4ce
Sam Georges <sam@daftspunk.com>
2014-03-31 21:08:06 +1100

    Write new API design for Network\Http protocol

--- src/Network/Http.php
+++ src/Network/Http.php
@@ -7,7 +7,52 @@
  *
  * @package october\network
  * @author Alexey Bobkov, Samuel Georges
+ *
+ * Usage:
+ * 
+ *   Http::get('http://octobercms.com');
+ *   Http::post('...');
+ *   Http::delete('...');
+ *   Http::patch('...');
+ *   Http::put('...');
+ *   Http::options('...');
+ *
+ *   $result = Http::post('http://octobercms.com');
+ *   echo $result;                          // Outputs: <html><head><title>...
+ *   echo $result->code;                    // Outputs: 200
+ *   echo $result->headers['Content-Type']; // Outputs: text/html; charset=UTF-8
+ *
+ *   Http::post('http://octobercms.com', function($http){
+ *
+ *       // Sets a HTTP header
+ *       $http->header('Rest-Key', '...');
+ *
+ *       // Set a proxy of type (http, socks4, socks5)
+ *       $http->proxy('type', 'host', 'port', 'username', 'password');
+ *
+ *       // Use basic authentication
+ *       $http->auth('user', 'pass');
+ *
+ *       // Sends data with the request
+ *       $http->data('foo', 'bar');
+ *       $http->data(['key' => 'value', ...]);
+ *
+ *       // Disable redirects
+ *       $http->noRedirect();
+ *
+ *       // Sets the timeout duration
+ *       $http->timeout(3600);
+ *
+ *       // Write response to a file
+ *       $http->toFile('some/path/to/a/file.txt');
+ *
+ *       // Sets a cURL option manually
+ *       $http->withOption('CURLOPT_SSL_VERIFYHOST', false);
+ *
+ *   });
+ *
  */
+
 class Http
 {
     const METHOD_GET = 'GET';
