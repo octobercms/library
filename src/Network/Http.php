bd34b59f3ad430fd0b1ed144e92774afe2258c58
Sam Georges <sam@daftspunk.com>
2014-04-01 19:14:35 +1100

    Build out class to new API specs

--- src/Network/Http.php
+++ src/Network/Http.php
@@ -39,6 +39,9 @@
  *
  *       // Disable redirects
  *       $http->noRedirect();
+ * *
+ *       // Check host SSL certificate
+ *       $http->verifySSL();
  *
  *       // Sets the timeout duration
  *       $http->timeout(3600);
@@ -47,7 +50,7 @@
  *       $http->toFile('some/path/to/a/file.txt');
  *
  *       // Sets a cURL option manually
- *       $http->withOption('CURLOPT_SSL_VERIFYHOST', false);
+ *       $http->setOption('CURLOPT_SSL_VERIFYHOST', false);
  *
  *   });
  *
@@ -63,312 +66,240 @@ class Http
     const METHOD_OPTIONS = 'OPTIONS';
 
     /**
-     * @var array The headers to be sent with the request.
+     * @var string The HTTP address to use.
      */
-    protected $headers = [];
-
-    /**
-     * @var array The headers to be sent with the request.
-     */
-    protected $options = [];
+    public $url;
 
     /**
      * @var string The method the request should use.
      */
-    protected $method;
+    public $method;
 
     /**
-     * @var string The last response in its original form.
+     * @var array The headers to be sent with the request.
      */
-    protected $response;
-
-    /**
-     * @var array The last response headers.
-     */
-    protected $responseHeaders;
-
-    /**
-     * @var array The results of curl_getinfo on the last request.
-     */
-    protected $responseInfo;
+    public $headers = [];
 
     /**
      * @var string The last response body.
      */
-    public $responseBody = '';
+    public $body = '';
+
+    /**
+     * @var string The last response body (without headers extracted).
+     */
+    public $rawBody = '';
 
     /**
      * @var array The last returned HTTP code.
      */
-    public $responseCode;
+    public $code;
 
     /**
-     * Execute the HTTP request.
+     * @var array The cURL response information.
+     */
+    public $info;
+
+    /**
+     * @var array cURL Options.
+     */
+    public $requestOptions;
+
+    /**
+     * @var array Request data.
+     */
+    public $requestData;
+
+    /**
+     * @var array Request headers.
+     */
+    public $requestHeaders;
+
+    /**
+     * @var string If writing response to a file, which file to use.
+     */
+    public $streamFile;
+
+    /**
+     * @var string If writing response to a file, which write filter to apply.
+     */
+    public $streamFilter;
+
+    /**
+     * Make the object with common properties
+     * @param string   $url     HTTP request address
+     * @param string   $method  Request method (GET, POST, PUT, DELETE, etc)
+     * @param callable $options Callable helper function to modify the object
+     */
+    public static function make($url, $method, $options = null)
+    {
+        $http = new self;
+        $http->url = $url;
+        $http->method = $method;
+        if ($options && is_callable($options)) $options($http);
+        return $http;
+    }
+
+    /**
+     * Make a HTTP GET call.
      * @param string $url
-     * @param array $options
-     * @return string response body
+     * @param array  $options
+     * @return self
      */
-    protected function send($url, $options = [])
+    public static function get($url, $options = null)
     {
-        /*
-         * Create and execute the cURL Resource
-         */
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_URL, $url);
-
-        if (count($this->options))
-            curl_setopt_array($curl, $this->options);
-
-        if (count($options))
-            curl_setopt_array($curl, $options);
-
-        if ($this->method == self::METHOD_POST)
-            curl_setopt($curl, CURLOPT_POST, 1);
-        elseif ($this->method !== self::METHOD_GET)
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $this->method);
-
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
-        $response = curl_exec($curl);
-
-        /*
-         * Extract the response info, header and body from a cURL response. Saves
-         * the data in variables stored on the object.
-         */
-        $this->response = $response;
-        $this->responseInfo = curl_getinfo($curl);
-        $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        $headerText = substr($response, 0, $headerSize);
-        $this->responseHeaders = $this->headerToArray($headerText);
-        $this->responseBody = substr($response, $headerSize);
-        $this->responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        curl_close($curl);
-        return $this;
+        $http = self::make($url, self::METHOD_GET, $options);
+        return $http->send();
     }
 
     /**
-     * Get all or a specific header from the last curl statement.
-     * @param string $header Name of the header to get. If not provided, gets
-     * all headers from the last response.
-     * @return array
-     */
-    public function getHeaders($header = null)
-    {
-        if (!$header) {
-            return $this->responseHeaders;
-        }
-
-        if (array_key_exists($header, $this->responseHeaders)) {
-            return $this->responseHeaders[$header];
-        }
-    }
-
-    //
-    // cURL settings
-    //
-
-    /**
-     * Add a header to the request.
-     * @param string $value
-     */
-    public function withHeader($value)
-    {
-        $this->headers[] = $value;
-        return $this;
-    }
-
-    /**
-     * Add a single option to the request.
-     * @param string $option
-     * @param string $value
-     */
-    public function withOption($option, $value)
-    {
-        $this->options[$option] = $value;
-        return $this;
-    }
-
-    /**
-     * Sets the request timeout.
-     * @param string $timeout
-     */
-    public function withTimeout($timeout)
-    {
-        $this->options[CURLOPT_CONNECTTIMEOUT] = $timeout;
-        $this->options[CURLOPT_TIMEOUT] = $timeout;
-        return $this;
-    }
-
-    /**
-     * Adds authentication to the comms.
-     * @param string $user
-     * @param string $pass
-     */
-    public function withAuth($user, $pass)
-    {
-        $this->options[CURLOPT_HTTPAUTH] = CURLAUTH_BASIC;
-        $this->options[CURLOPT_USERPWD] = $user . ':' . $pass;
-        return $this;
-    }
-
-    /**
-     * Disable follow location (redirects)
-     */
-    public function noRedirect()
-    {
-        $this->options[CURLOPT_FOLLOWLOCATION] = false;
-        return $this;
-    }
-
-    /**
-     * Sets a proxy to use with this request
-     */
-    public function withProxy($type, $host, $port, $username = null, $password = null)
-    {
-        if ($type === 'http')
-            $this->options[CURLOPT_PROXYTYPE] = CURLPROXY_HTTP;
-        elseif ($type === 'socks4')
-            $this->options[CURLOPT_PROXYTYPE] = CURLPROXY_SOCKS5;
-        elseif ($type === 'socks5')
-            $this->options[CURLOPT_PROXYTYPE] = CURLPROXY_SOCKS5;
-
-        $this->options[CURLOPT_PROXY] = $host . ':' . $port;
-
-        if ($username && $password)
-            $this->options[CURLOPT_PROXYUSERPWD] = $username . ':' . $password;
-
-        return $this;
-    }
-
-    //
-    // Protocol methods
-    //
-
-    /**
-     * Make a HTTP GET call
+     * Make a HTTP POST call.
      * @param string $url
-     * @param array $query GET parameters/query string, optional
-     * @param array $options cURL options (curl_setopt_array), optional
-     * @return string
+     * @param array  $options
+     * @return self
      */
-    public function get($url, $query = [], $options = [])
+    public static function post($url, $options = null)
     {
-        $this->method = self::METHOD_GET;
-        if (!empty($query)) $url = $this->buildUrl($url, $query);
-
-        return $this->send($url, $options);
-    }
-
-    /**
-     * Make a HTTP POST call
-     * @param string $url
-     * @param array $data POST data, optional
-     * @param array $options cURL options (curl_setopt_array), optional
-     * @return string response body
-     */
-    public function post($url, $data = [], $options = [])
-    {
-        $this->method = self::METHOD_POST;
-        if (!empty($data)) $this->setPostData($data);
-
-        return $this->send($url, $options);
+        $http = self::make($url, self::METHOD_POST, $options);
+        return $http->send();
     }
 
     /**
      * Make a HTTP DELETE call.
      * @param string $url
-     * @param array $options cURL options (curl_setopt_array), optional
-     * @return string response body
+     * @param array  $options
+     * @return self
      */
-    public function delete($url, $options = [])
+    public static function delete($url, $options = null)
     {
-        $this->method = self::METHOD_DELETE;
-        return $this->send($url, $options);
+        $http = self::make($url, self::METHOD_DELETE, $options);
+        return $http->send();
     }
 
     /**
      * Make a HTTP PATCH call.
      * @param string $url
-     * @param array $data POST data, optional
-     * @param array $options cURL options (curl_setopt_array), optional
-     * @return string response body
+     * @param array  $options
+     * @return self
      */
-    public function patch($url, $data = [], $options = [])
+    public static function patch($url, $options = null)
     {
-        $this->method = self::METHOD_PATCH;
-        if (!empty($data)) $this->setPostData($data);
-
-        return $this->send($url, $options);
+        $http = self::make($url, self::METHOD_PATCH, $options);
+        return $http->send();
     }
 
     /**
      * Make a HTTP PUT call.
      * @param string $url
-     * @param array $data POST data, optional
-     * @param array $options cURL options (curl_setopt_array), optional
-     * @return string response body
+     * @param array  $options
+     * @return self
      */
-    public function put($url, $data = [], $options = [])
+    public static function put($url, $options = null)
     {
-        $this->method = self::METHOD_PUT;
-        if (!empty($data)) $this->setPostData($data);
-        return $this->send($url, $options);
+        $http = self::make($url, self::METHOD_PUT, $options);
+        return $http->send();
     }
 
     /**
      * Make a HTTP OPTIONS call.
      * @param string $url
-     * @param array $options cURL options (curl_setopt_array), optional
+     * @param array  $options
+     * @return self
+     */
+    public static function options($url, $options = null)
+    {
+        $http = self::make($url, self::METHOD_OPTIONS, $options);
+        return $http->send();
+    }
+
+    /**
+     * Execute the HTTP request.
      * @return string response body
      */
-    public function options($url, $options = [])
-    {
-        $this->method = self::METHOD_OPTIONS;
-        return $this->send($url, $options);
-    }
-
-    //
-    // Helpers
-    //
-
-    /**
-     * Get info about the last executed curl statement.
-     * @return mixed
-     */
-    public function getResponseInfo()
-    {
-        return $this->responseInfo;
-    }
-
-    /**
-     * Build an URL with an optional query string.
-     * @param string $url the base URL without any query string
-     * @param array $query array of GET parameters
-     * @return string
-     */
-    public function buildUrl($url, $query)
+    public function send()
     {
         /*
-         * Append query string
+         * Create and execute the cURL Resource
          */
-        if (!empty($query)) {
-            $queryString = http_build_query($query);
-            $url .= '?' . $queryString;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->url);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+        if ($this->requestOptions && is_array($this->requestOptions))
+            curl_setopt_array($curl, $this->requestOptions);
+
+        /*
+         * Set request method
+         */
+        if ($this->method == self::METHOD_POST) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, '');
+        }
+        elseif ($this->method !== self::METHOD_GET)
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $this->method);
+
+        /*
+         * Set request data
+         */
+        if ($this->requestData) {
+            if (in_array($this->method, [self::METHOD_POST, self::METHOD_PATCH, self::METHOD_PUT])) {
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $this->requestData);
+            }
+            elseif ($this->method == self::METHOD_GET) {
+                curl_setopt($curl, CURLOPT_URL, $this->url . '?' . http_build_query($this->requestData));
+            }
         }
 
-        return $url;
-    }
+        /*
+         * Set request headers
+         */
+        if ($this->requestHeaders) {
+            $requestHeaders = [];
+            foreach ($this->requestHeaders as $key => $value) {
+                $requestHeaders[] = $key . ': ' . $value;
+            }
 
-    /**
-     * Set the POST data of the call.
-     * @param $data
-     */
-    protected function setPostData($data)
-    {
-        $postData = http_build_query($data);
-        $this->options[CURLOPT_POSTFIELDS] = $postData;
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $requestHeaders);
+        }
+
+        /*
+         * Handle output to file
+         */
+        if ($this->streamFile) {
+            $stream = fopen($this->streamFile, 'w');
+            if ($this->streamFilter) {
+                stream_filter_append($stream, $this->streamFilter, STREAM_FILTER_WRITE);
+            }
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_FILE, $stream);
+            curl_exec($curl);
+        }
+        /*
+         * Handle output to variable
+         */
+        else {
+            $response = $this->rawBody = curl_exec($curl);
+            $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+            $this->headers = $this->headerToArray(substr($response, 0, $headerSize));
+            $this->body = substr($response, $headerSize);
+        }
+
+        $this->info = curl_getinfo($curl);
+        $this->code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        /*
+         * Close resources
+         */
+        curl_close($curl);
+
+        if ($this->streamFile)
+            fclose($stream);
+
+        return $this;
     }
 
     /**
@@ -376,11 +307,11 @@ class Http
      * @param string $header
      * @return array
      */
-    protected function headerToArray($header)
+    private function headerToArray($header)
     {
-        $tmp = explode("\r\n", $header);
         $headers = [];
-        foreach ($tmp as $singleHeader) {
+        $parts = explode("\r\n", $header);
+        foreach ($parts as $singleHeader) {
             $delimiter = strpos($singleHeader, ': ');
             if ($delimiter !== false) {
                 $key = substr($singleHeader, 0, $delimiter);
@@ -399,11 +330,144 @@ class Http
     }
 
     /**
+     * Add a data to the request.
+     * @param string $value
+     */
+    public function data($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $_key => $_value) {
+                $this->data($_key, $_value);
+            }
+            return;
+        }
+
+        $this->requestData[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * Add a header to the request.
+     * @param string $value
+     */
+    public function header($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $_key => $_value) {
+                $this->header($_key, $_value);
+            }
+            return;
+        }
+
+        $this->requestHeaders[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * Sets a proxy to use with this request
+     */
+    public function proxy($type, $host, $port, $username = null, $password = null)
+    {
+        if ($type === 'http')
+            $this->setOption(CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+        elseif ($type === 'socks4')
+            $this->setOption(CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+        elseif ($type === 'socks5')
+            $this->setOption(CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+
+        $this->setOption(CURLOPT_PROXY, $host . ':' . $port);
+
+        if ($username && $password)
+            $this->setOption(CURLOPT_PROXYUSERPWD, $username . ':' . $password);
+
+        return $this;
+    }
+
+    /**
+     * Adds authentication to the comms.
+     * @param string $user
+     * @param string $pass
+     */
+    public function auth($user, $pass = null)
+    {
+        if (strpos($user, ':') !== null && !$pass)
+            list($user, $pass) = explode(':', $user);
+
+        $this->setOption(CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        $this->setOption(CURLOPT_USERPWD, $user . ':' . $pass);
+        return $this;
+    }
+
+    /**
+     * Disable follow location (redirects)
+     */
+    public function noRedirect()
+    {
+        $this->setOption(CURLOPT_FOLLOWLOCATION, false);
+        return $this;
+    }
+
+    /**
+     * Enable SSL verification
+     */
+    public function verifySSL()
+    {
+        $this->setOption(CURLOPT_SSL_VERIFYPEER, true);
+        $this->setOption(CURLOPT_SSL_VERIFYHOST, true);
+        return $this;
+    }
+
+    /**
+     * Sets the request timeout.
+     * @param string $timeout
+     */
+    public function timeout($timeout)
+    {
+        $this->setOptions(CURLOPT_CONNECTTIMEOUT, $timeout);
+        $this->setOptions(CURLOPT_TIMEOUT, $timeout);
+        return $this;
+    }
+
+    /**
+     * Write the response to a file
+     * @param  string $path   Path to file
+     * @param  string $filter Stream filter as listed in stream_get_filters()
+     * @return self
+     */
+    public function toFile($path, $filter = null)
+    {
+        $this->streamFile = $path;
+
+        if ($filter)
+            $this->streamFilter = $filter;
+
+        return $this;
+    }
+
+    /**
+     * Add a single option to the request.
+     * @param string $option
+     * @param string $value
+     */
+    public function setOption($option, $value = null)
+    {
+        if (is_array($option)) {
+            foreach ($option as $_option => $_value) {
+                $this->setOption($_option, $_value);
+            }
+            return;
+        }
+
+        $this->requestOptions[$option] = $value;
+        return $this;
+    }
+
+    /**
      * Handy if this object is called directly.
      * @return string The last response.
      */
     public function __toString()
     {
-        return (string)$this->responseBody;
+        return (string)$this->body;
     }
 }
