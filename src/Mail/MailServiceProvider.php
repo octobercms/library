ec62806709acd35d5a700ba03e1d93ccd6961bc0
Sam Georges <sam@daftspunk.com>
2014-05-05 13:33:40 +1000

    Extended Mail sender

---
+++ src/Mail/MailServiceProvider.php
@@ -0,0 +1,54 @@
+<?php namespace October\Rain\Mail;
+
+use Illuminate\Mail\MailServiceProvider as MailServiceProviderBase;
+
+class MailServiceProvider extends MailServiceProviderBase
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('mailer', function($app) {
+
+            $this->registerSwiftMailer();
+
+            // Once we have create the mailer instance, we will set a container instance
+            // on the mailer. This allows us to resolve mailer classes via containers
+            // for maximum testability on said classes instead of passing Closures.
+            $mailer = new Mailer($app['view'], $app['swift.mailer']);
+
+            $mailer->setLogger($app['log'])->setQueue($app['queue']);
+
+            $mailer->setContainer($app);
+
+            // If a "from" address is set, we will set it on the mailer so that all mail
+            // messages sent by the applications will utilize the same "from" address
+            // on each one, which makes the developer's life a lot more convenient.
+            $from = $app['config']['mail.from'];
+
+            if (is_array($from) && isset($from['address']))
+            {
+                $mailer->alwaysFrom($from['address'], $from['name']);
+            }
+
+            // Here we will determine if the mailer should be in "pretend" mode for this
+            // environment, which will simply write out e-mail to the logs instead of
+            // sending it over the web, which is useful for local dev environments.
+            $pretend = $app['config']->get('mail.pretend', false);
+
+            $mailer->pretend($pretend);
+
+            return $mailer;
+        });
+    }
+}
---
+++ src/Mail/Mailer.php
@@ -0,0 +1,42 @@
+<?php namespace October\Rain\Mail;
+
+use Illuminate\Mail\Mailer as MailerBase;
+
+/**
+ * Mailer class for sending mail.
+ *
+ * @package october\mail
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class Mailer extends MailerBase
+{
+
+    /**
+     * Add the content to a given message.
+     *
+     * @param  \Illuminate\Mail\Message  $message
+     * @param  string  $view
+     * @param  string  $plain
+     * @param  array   $data
+     * @return void
+     */
+    protected function addContent($message, $view, $plain, $data)
+    {
+        if (isset($view)) {
+            $viewContent = $this->getView($view, $data);
+            $result = MailParser::parse($viewContent);
+            $message->setBody($result['html'], 'text/html');
+
+            if ($result['text'])
+                $plain = $result['text'];
+
+            if ($subject = array_get($result['settings'], 'subject'))
+                $message->subject($subject);
+        }
+
+        if (isset($plain)) {
+            $message->addPart($this->getView($plain, $data), 'text/plain');
+        }
+    }
+
+}
