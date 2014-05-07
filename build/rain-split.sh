b131c07431f497d713f71e0854aff9a5b028c6dd
Sam Georges <sam@daftspunk.com>
2014-05-07 17:55:00 +1000

    Add mail component to build

--- build/rain-split.sh
+++ build/rain-split.sh
@@ -7,6 +7,7 @@
 ./git-subsplit.sh publish --no-tags src/Support:git@github.com:octoberrain/support.git
 ./git-subsplit.sh publish --no-tags src/Router:git@github.com:octoberrain/router.git
 ./git-subsplit.sh publish --no-tags src/Network:git@github.com:octoberrain/network.git
+./git-subsplit.sh publish --no-tags src/Mail:git@github.com:octoberrain/mail.git
 ./git-subsplit.sh publish --no-tags src/Html:git@github.com:octoberrain/html.git
 ./git-subsplit.sh publish --no-tags src/Filesystem:git@github.com:octoberrain/filesystem.git
 ./git-subsplit.sh publish --no-tags src/Extension:git@github.com:octoberrain/extension.git
