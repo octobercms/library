19e551325dba5d94b23d07d1867c386916ba970b
Sam Georges <sam@daftspunk.com>
2014-05-19 19:06:10 +1000

    Minor

--- src/Support/Markdown.php
+++ src/Support/Markdown.php
@@ -10,10 +10,14 @@ use ParsedownExtra;
  */
 class Markdown
 {
+    /**
+     * Parse text using Markdown and Markdown-Extra
+     * @param  string $text Mardown text to parse
+     * @return string       Resulting HTML
+     */
     public static function parse($text)
     {
         $instance = new ParsedownExtra;
-
         return $instance->text($text);
     }
 }
