1c914b1a98f83dd6f93b684fa3c0d2c81e9c0850
Peter O <that0n3guy@users.noreply.github.com>
2014-05-30 12:18:29 -0500

    Added morphToMany and morphedByMany polymorphic relationship support

---
+++ src/Database/Relations/MorphToMany.php
@@ -0,0 +1,12 @@
+<?php namespace October\Rain\Database\Relations;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphToMany as MorphToManyBase;
+
+class MorphToMany extends MorphToManyBase
+{
+    use BelongsToManyTrait;
+}
+
+
