736b4a983a1a6d258399fff6b912d1a4e9140065
Sam Georges <sam@daftspunk.com>
2014-06-06 21:33:30 +1000

    Adds traceLog() helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -28,3 +28,20 @@ if (!function_exists('post'))
         return Input::get($dottedName, $default);
     }
 }
+
+if (!function_exists('traceLog'))
+{
+    /**
+     * Writes a trace message to a log file.
+     * @param mixed $message Specifies a message to log. The message can be an object, array or string.
+     * @param string $level Specifies a level to use. If this parameter is omitted, the default listener will be used (info).
+     * @return void
+     */
+    function traceLog($message, $level = 'info')
+    {
+        if (is_array($message) || is_object($message))
+            $message = print_r($message, true);
+
+        forward_static_call(['Log', $level], $message);
+    }
+}
