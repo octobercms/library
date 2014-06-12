9b05cf9fd8c92df4bfe7ef4b4368ff0d0cb4a51c
Sam Georges <sam@daftspunk.com>
2014-06-12 18:42:28 +1000

    Adds traceSql() helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -45,3 +45,32 @@ if (!function_exists('traceLog'))
         Log::$level($message);
     }
 }
+
+if (!function_exists('traceSql'))
+{
+    /**
+     * Begins to monitor all SQL output.
+     * @return void
+     */
+    function traceSql()
+    {
+        Event::listen('illuminate.query', function($query, $bindings, $time, $name)
+        {
+            $data = compact('bindings', 'time', 'name');
+
+            foreach ($bindings as $i => $binding){
+
+                if ($binding instanceof \DateTime)
+                    $bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+
+                else if (is_string($binding))
+                    $bindings[$i] = "'$binding'";
+            }
+
+            $query = str_replace(array('%', '?'), array('%%', '%s'), $query);
+            $query = vsprintf($query, $bindings);
+
+            traceLog($query);
+        });
+    }
+}
