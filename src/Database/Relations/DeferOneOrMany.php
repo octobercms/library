a534441564429a650143af0e5b761d26837062e5
Dan Herd <dan@meanstreak.co.uk>
2014-11-12 11:15:41 +0000

    Ensure db table prefix is used in raw SQL queries

--- src/Database/Relations/DeferOneOrMany.php
+++ src/Database/Relations/DeferOneOrMany.php
@@ -26,7 +26,7 @@ trait DeferOneOrMany
             // Bind (Add)
             $query = $query->orWhereExists(function($query) use ($sessionKey) {
                 $query->from('deferred_bindings')
-                    ->whereRaw(DbDongle::cast('slave_id', 'INTEGER').' = '.$this->related->getQualifiedKeyName())
+                    ->whereRaw(DbDongle::cast('slave_id', 'INTEGER').' = '.DbDongle::getTablePrefix().$this->related->getQualifiedKeyName())
                     ->where('master_field', $this->relationName)
                     ->where('master_type', get_class($this->parent))
                     ->where('session_key', $sessionKey)
@@ -37,13 +37,13 @@ trait DeferOneOrMany
         // Unbind (Remove)
         $newQuery->whereNotExists(function($query) use ($sessionKey) {
             $query->from('deferred_bindings')
-                ->whereRaw(DbDongle::cast('slave_id', 'INTEGER').' = '.$this->related->getQualifiedKeyName())
+                ->whereRaw(DbDongle::cast('slave_id', 'INTEGER').' = '.DbDongle::getTablePrefix().$this->related->getQualifiedKeyName())
                 ->where('master_field', $this->relationName)
                 ->where('master_type', get_class($this->parent))
                 ->where('session_key', $sessionKey)
                 ->where('is_bind', false)
-                ->whereRaw(DbDongle::parse('id > ifnull((select max(id) from deferred_bindings where
-                        '.DbDongle::cast('slave_id', 'INTEGER').' = '.$this->related->getQualifiedKeyName().' and
+                ->whereRaw(DbDongle::parse('id > ifnull((select max(id) from '.DbDongle::getTablePrefix().'deferred_bindings where
+                        '.DbDongle::cast('slave_id', 'INTEGER').' = '.DbDongle::getTablePrefix().$this->related->getQualifiedKeyName().' and
                         master_field = ? and
                         master_type = ? and
                         session_key = ? and
