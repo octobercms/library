4f7274b59a8d46e894ffa6d363721e5a4f932651
Samuel Georges <sam@daftspunk.com>
2015-02-14 13:26:24 +1100

    *** REMOVED RELATIVE LINK POLICY *** A relative link policy was a bad idea, Laravel/Redirects did not play nicely at all.

--- src/Html/UrlServiceProvider.php
+++ src/Html/UrlServiceProvider.php
@@ -16,63 +16,13 @@ class UrlServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerUrlGenerator();
-
-        if (!$this->app->runningInConsole() || $this->app->runningUnitTests()) {
-            $this->setUrlGeneratorPolicy();
-        }
+        $this->setUrlGeneratorPolicy();
     }
 
-    /**
-     * Register the URL generator service.
-     * @return void
-     */
-    protected function registerUrlGenerator()
-    {
-        $this->app['url'] = $this->app->share(function($app) {
-
-            if (!isset($app['routes'])) {
-                $routes = $app['router']->getRoutes();
-                $app->instance('routes', $routes);
-            }
-            else {
-                $routes = $app['routes'];
-            }
-
-            $request = $app->rebinding('request', $this->requestRebinder());
-
-            $url = new UrlGenerator($routes, $request);
-
-            $url->setSessionResolver(function() {
-                return $this->app['session'];
-            });
-
-            // If the route collection is "rebound", for example, when the routes stay
-            // cached for the application, we will need to rebind the routes on the
-            // URL generator instance so it has the latest version of the routes.
-            $app->rebinding('routes', function($app, $routes) {
-                $app['url']->setRoutes($routes);
-            });
-
-            return $url;
-        });
-    }
-
-    /**
-     * Get the URL generator request rebinder.
-     * @return \Closure
-     */
-    protected function requestRebinder()
-    {
-        return function($app, $request) {
-            $app['url']->setRequest($request);
-        };
-    }
 
     /**
      * Controls how URL links are generated throughout the application.
      *
-     * relative - relative to the application, schema and hostname is omitted
      * detect   - detect hostname and use the current schema
      * secure   - detect hostname and force HTTPS schema
      * insecure - detect hostname and force HTTP schema
@@ -80,12 +30,9 @@ class UrlServiceProvider extends ServiceProvider
      */
     public function setUrlGeneratorPolicy()
     {
-        $policy = $this->app['config']->get('cms.linkPolicy', 'relative');
+        $policy = $this->app['config']->get('cms.linkPolicy', 'detect');
 
         switch (strtolower($policy)) {
-            case 'detect':
-                // Do nothing
-                break;
             case 'force':
                 $appUrl = $this->app['config']->get('app.url');
                 $this->app['url']->forceRootUrl($appUrl);
@@ -96,19 +43,7 @@ class UrlServiceProvider extends ServiceProvider
             case 'secure':
                 $this->app['url']->forceSchema('https');
                 break;
-            case 'relative':
-                $this->app['url']->forceRelative();
-                break;
         }
-    }
-
-    /**
-     * Get the services provided by the provider.
-     * @return array
-     */
-    public function provides()
-    {
-        return ['url'];
     }
 
 }
