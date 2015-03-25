02edd71f512aecc0d2139417fd6c5aedb96934bd
Łukasz Biały <lukasz@c-call.eu>
2015-03-25 03:58:46 +0100

    fixed Validation trait static event bindings failing during tests, referred to issue #78

--- src/Database/Traits/Validation.php
+++ src/Database/Traits/Validation.php
@@ -67,18 +67,6 @@ trait Validation
 
             }, 500);
         });
-
-        static::validating(function($model) {
-            $model->fireEvent('model.beforeValidate');
-            if ($model->methodExists('beforeValidate'))
-                $model->beforeValidate();
-        });
-
-        static::validated(function($model) {
-            $model->fireEvent('model.afterValidate');
-            if ($model->methodExists('afterValidate'))
-                $model->afterValidate();
-        });
     }
 
     /**
@@ -115,7 +103,7 @@ trait Validation
             ? $this->throwOnValidation
             : true;
 
-        if ($this->fireModelEvent('validating') === false) {
+        if ($this->fireEvent('model.beforeValidate') === false) {
             if ($throwOnValidation) {
                 throw new ModelException($this);
             }
@@ -123,6 +111,9 @@ trait Validation
                 return false;
             }
         }
+
+        if ($this->methodExists('beforeValidate'))
+            $this->beforeValidate();
 
         /*
          * Perform validation
@@ -227,7 +218,9 @@ trait Validation
             }
         }
 
-        $this->fireModelEvent('validated', false);
+        $this->fireEvent('model.afterValidate');
+        if ($this->methodExists('afterValidate'))
+            $this->afterValidate();
 
         if (!$success && $throwOnValidation) {
             throw new ModelException($this);
@@ -320,26 +313,6 @@ trait Validation
     public function errors()
     {
         return $this->validationErrors;
-    }
-
-    /**
-     * Create a new native event for handling beforeValidate().
-     * @param Closure|string $callback
-     * @return void
-     */
-    public static function validating($callback)
-    {
-        static::registerModelEvent('validating', $callback);
-    }
-
-    /**
-     * Create a new native event for handling afterValidate().
-     * @param Closure|string $callback
-     * @return void
-     */
-    public static function validated($callback)
-    {
-        static::registerModelEvent('validated', $callback);
     }
 
 }
