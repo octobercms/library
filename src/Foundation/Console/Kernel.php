835e91c5e208ff41fca142ea1ccd8abe24c2cf16
Samuel Georges <sam@daftspunk.com>
2015-05-19 20:56:02 +1000

    This issue is fixed in Laravel v5.0.31 https://github.com/laravel/framework/commit/125809bc364159773c09e5f480cff7f29a89fdea

--- src/Foundation/Console/Kernel.php
+++ src/Foundation/Console/Kernel.php
@@ -26,26 +26,6 @@ class Kernel extends ConsoleKernel
     ];
 
     /**
-     * This entire method should be removed when we find out what is causing
-     * the issue in Laravel 5.0.22. We can at least revert the breaking logic
-     * for now. See comment here:
-     * - https://github.com/laravel/framework/commit/6a9aa29278e13274549d8205f2b21a2d2cb70e98
-     */
-    public function __construct(\Illuminate\Contracts\Foundation\Application $app, \Illuminate\Contracts\Events\Dispatcher $events)
-    {
-        $this->app = $app;
-        $this->events = $events;
-
-        //
-        // This causes the kerenel to boot twice...
-        //
-        // $this->app->booted(function()
-        // {
-        $this->defineConsoleSchedule();
-        // });
-    }
-
-    /**
      * The Artisan commands provided by your application.
      *
      * @var array
