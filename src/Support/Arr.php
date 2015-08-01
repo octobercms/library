8ee03e1e8658e00c365e884e3dcc50c02ba8d7ea
Samuel Georges <sam@daftspunk.com>
2015-08-01 11:34:41 +1000

    Add proxy class for Array helper

---
+++ src/Support/Arr.php
@@ -0,0 +1,14 @@
+<?php namespace October\Rain\Support;
+
+use Illuminate\Support\Arr as ArrHelper;
+
+/**
+ * Array helper
+ *
+ * @package october\support
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class Arr extends ArrHelper
+{
+
+}
