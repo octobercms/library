f44cad586efa6bb6eb20011e6ecb806e2f6180bf
Samuel Georges <sam@daftspunk.com>
2015-09-19 05:36:20 +1000

    Create new Revisionable trait

---
+++ src/Database/Models/Revision.php
@@ -0,0 +1,44 @@
+<?php namespace October\Rain\Database\Models;
+
+use Db;
+use October\Rain\Database\Model;
+
+/**
+ * Revision Model
+ *
+ * @package october\database
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class Revision extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'revisions';
+
+    /**
+     * Returns "new value" casted as the saved type.
+     * @return mixed
+     */
+    public function getNewValueAttribute($value)
+    {
+        if ($this->cast == 'date') {
+            return $this->asDateTime($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Returns "old value" casted as the saved type.
+     * @return mixed
+     */
+    public function getOldValueAttribute($value)
+    {
+        if ($this->cast == 'date') {
+            return $this->asDateTime($value);
+        }
+
+        return $value;
+    }
+}
