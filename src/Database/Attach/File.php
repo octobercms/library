d6ce5d8145770a08f1f3f37c4aea70f07f211b7b
Genius Soluções Web <douglas@estudiogenius.com.br>
2015-11-24 03:10:10 -0200

    Resize images on upload to default values
    Resizes images on upload to a default ImageMaxHeight and Width supplied on config/cms.php
    This will prevent too big images in the server
    //...
        'storage' => [
            'uploads' => [
                'disk'   => 'local',
                'folder' => 'uploads',
                'path'   => '/storage/app/uploads',
                'imageMaxWidth' => 2000,
                'imageMaxHeight' => 2000,
            ],
    //...

--- src/Database/Attach/File.php
+++ src/Database/Attach/File.php
@@ -1,5 +1,6 @@
 <?php namespace October\Rain\Database\Attach;
 
+use Config;
 use Storage;
 use File as FileHelper;
 use October\Rain\Database\Model;
@@ -553,6 +554,19 @@ class File extends Model
         }
 
         $destinationPath = $this->getStorageDirectory() . $this->getPartitionDirectory();
+
+        if ($this->isImage()) {
+
+            $imageMaxWidth = Config::get('cms.storage.uploads.imageMaxWidth',0);
+            $imageMaxHeight = Config::get('cms.storage.uploads.imageMaxHeight',0);
+            $resizer = Resizer::open($sourcePath);
+
+            if (($imageMaxWidth && $resizer->getWidth() > $imageMaxWidth) || ($imageMaxHeight && $resizer->getHeight() > $imageMaxHeight)){
+
+                $resizer->resize($imageMaxWidth,$imageMaxHeight);
+                $resizer->save($sourcePath);
+            }
+        }
 
         if (!$this->isLocalStorage()) {
             return $this->copyLocalToStorage($sourcePath, $destinationPath . $destinationFileName);
