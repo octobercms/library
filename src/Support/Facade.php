2ef58e9a93756deda83fc995bfbbc82e1b909f5f
Samuel Georges <sam@daftspunk.com>
2015-12-19 09:56:35 +1100

    Add support for IoC helper + singleton patterns

--- src/Support/Facade.php
+++ src/Support/Facade.php
@@ -4,12 +4,38 @@ use Illuminate\Support\Facades\Facade as FacadeParent;
 
 /**
  * Facade base class
- * Reserved for later use.
+ * Adds the ability to define a fallback instance.
  *
  * @package october\support
  * @author Alexey Bobkov, Samuel Georges
  */
 class Facade extends FacadeParent
 {
+
+    /**
+     * {@inheritDoc}
+     */
+    protected static function resolveFacadeInstance($name)
+    {
+        if (
+            !is_object($name) &&
+            !static::$app->bound($name) &&
+            ($instance = static::getFacadeInstance()) !== null
+        ) {
+            static::$app->instance($name, $instance);
+        }
+
+        return parent::resolveFacadeInstance($name);
+    }
+
+    /**
+     * If the accessor is not found via getFacadeAccessor, use this instance as a fallback.
+     *
+     * @return mixed
+     */
+    protected static function getFacadeInstance()
+    {
+        return null;
+    }
 
 }
---
+++ src/Support/Singleton.php
@@ -0,0 +1,67 @@
+<?php namespace October\Rain\Support;
+
+use App; // @todo Allow external binding
+
+/**
+ * IoC Singleton class.
+ *
+ * A self binding, self contained single class that supports IoC.
+ * Usage: myObject::instance()
+ *
+ * @package october\support
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class Singleton
+{
+    /**
+     * Create a new instance of this singleton.
+     */
+    final public static function instance()
+    {
+        $accessor = static::getSingletonAccessor();
+
+        if (!App::bound($accessor)) {
+            App::singleton($accessor, function() {
+                return static::getSingletonInstance();
+            });
+        }
+
+        return App::make($accessor);
+    }
+
+    /**
+     * This should be a meaningful IoC container code. Eg: backend.helper
+     */
+    protected static function getSingletonAccessor()
+    {
+        return get_called_class();
+    }
+
+    final public static function getSingletonInstance()
+    {
+        return new static;
+    }
+
+    /**
+     * Constructor.
+     */
+    final protected function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * Initialize the singleton free from constructor parameters.
+     */
+    protected function init() {}
+
+    public function __clone()
+    {
+        trigger_error('Cloning '.__CLASS__.' is not allowed.', E_USER_ERROR);
+    }
+
+    public function __wakeup()
+    {
+        trigger_error('Unserializing '.__CLASS__.' is not allowed.', E_USER_ERROR);
+    }
+}
