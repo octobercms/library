3f9284d871188158953f62b540c59fe011633dfd
Samuel Georges <sam@daftspunk.com>
2016-01-12 21:01:59 +1100

    Add support for X = true in SQLite

--- src/Database/Dongle.php
+++ src/Database/Dongle.php
@@ -65,6 +65,7 @@ class Dongle
         $sql = $this->parseGroupConcat($sql);
         $sql = $this->parseConcat($sql);
         $sql = $this->parseIfNull($sql);
+        $sql = $this->parseBooleanExpression($sql);
         return $sql;
     }
 
@@ -143,6 +144,25 @@ class Dongle
         }
 
         return str_ireplace('ifnull(', 'coalesce(', $sql);
+    }
+
+    /**
+     * Transforms true|false expressions in a statement.
+     * @param  string $sql
+     * @return string
+     */
+    public function parseBooleanExpression($sql)
+    {
+        if ($this->driver != 'sqlite') {
+            return $sql;
+        }
+
+        return preg_replace_callback('/(\w+)\s*(=|<>)\s*(true|false)($|\s)/i', function ($matches) {
+            array_shift($matches);
+            $space = array_pop($matches);
+            $matches[2] = $matches[2] == 'true' ? 1 : 0;
+            return implode(' ', $matches) . $space;
+        }, $sql);
     }
 
     /**
