9566b35941637281694221109850632df6b9faca
Sajjad <sajjad.servatjoo@gmail.com>
2016-01-28 19:37:29 +0330

    unicode character for slugs

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -104,4 +104,24 @@ class Str extends StrHelper
         return Html::clean($string);
     }
 
+
+    /**
+     * Generate a URL friendly "slug" from a given string.
+     *
+     * @param  string  $title
+     * @param  string  $separator
+     * @return string
+     */
+    public static function slug($title, $separator = '-')
+    {
+        // Convert all dashes/underscores into separator
+        $flip = $separator == '-' ? '_' : '-';
+        $title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
+        // Remove all characters that are not the separator, letters, numbers, or whitespace.
+        $title = urlencode($title);
+        // Replace all separator characters and whitespace by a single separator
+        $title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
+        return trim($title, $separator);
+    }
+
 }
