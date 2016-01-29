65052c9262c7faf0cbb5a5797374e4395a98c2d0
Pásztor Gábor <gpasztor87@gmail.com>
2016-01-29 13:18:09 +0100

    Add UserInterface

---
+++ src/Auth/UserInterface.php
@@ -0,0 +1,42 @@
+<?php namespace October\Rain\Auth;
+
+interface UserInterface
+{
+    /**
+     * Get the unique identifier for the user.
+     * @return mixed
+     */
+    public function getAuthIdentifier();
+
+    /**
+     * Get the password for the user.
+     * @return string
+     */
+    public function getAuthPassword();
+
+    /**
+     * Get the e-mail address where password reminders are sent.
+     * @return string
+     */
+    public function getReminderEmail();
+
+    /**
+     * Get the token value for the "remember me" session.
+     * @return string
+     */
+    public function getRememberToken();
+
+    /**
+     * Set the token value for the "remember me" session.
+     * @param  string $value
+     * @return void
+     */
+    public function setRememberToken($value);
+
+    /**
+     * Get the column name for the "remember me" token.
+     * @return string
+     */
+    public function getRememberTokenName();
+
+}
