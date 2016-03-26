633de6be90690f2c6cf019f6e6c96f1b45b203a2
jBOKA <jakobjfischer@googlemail.com>
2016-03-26 10:16:59 +0100

    Resizer sharpen - typo

--- src/Database/Attach/Resizer.php
+++ src/Database/Attach/Resizer.php
@@ -229,7 +229,7 @@ class Resizer
         $image = $this->image;
 
         // Normalize sharpening value
-        $kernelCenter = exp((80 - floatval($sharpen)) / 18) + 9;
+        $kernelCenter = exp((80 - floatval($sharpness)) / 18) + 9;
 
         $matrix = array(
             [-1, -1, -1],
