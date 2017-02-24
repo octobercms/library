d7a6f5fe268ed3fc94c75d1fde93bd5eb81e669b
Alexandru Constantin <alex@devmotion.ro>
2017-02-24 08:14:18 +0200

    Updated thumb filename to avoid adblockers
    Using a filename like "thumb_101_180x300_0_0_crop.jpg" will cause some ad blockers to block the image download (see here also http://stackoverflow.com/questions/17255493/how-to-stop-adblock-plus-blocking-images-in-html-page).
    Simply removing the "x" solved the problem in my case.

--- src/Database/Attach/File.php
+++ src/Database/Attach/File.php
@@ -400,7 +400,7 @@ class File extends Model
      */
     protected function getThumbFilename($width, $height, $options)
     {
-        return 'thumb_' . $this->id . '_' . $width . 'x' . $height . '_' . $options['offset'][0] . '_' . $options['offset'][1] . '_' . $options['mode'] . '.' . $options['extension'];
+        return 'thumb_' . $this->id . '_' . $width . $height . '_' . $options['offset'][0] . '_' . $options['offset'][1] . '_' . $options['mode'] . '.' . $options['extension'];
     }
 
     /**
