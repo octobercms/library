531e2111df034d52bab9598e05a560ccbbd5a161
Samuel Georges <sam@daftspunk.com>
2017-05-12 07:39:25 +1000

    share -> singleton

--- src/Flash/FlashServiceProvider.php
+++ src/Flash/FlashServiceProvider.php
@@ -16,7 +16,7 @@ class FlashServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app['flash'] = $this->app->share(function($app) {
+        $this->app->singleton('flash', function($app) {
             return new FlashBag;
         });
     }
