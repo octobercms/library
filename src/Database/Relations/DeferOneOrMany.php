c48d0b417bd646bb7f3641b9eea0bbb5828aacad
Samuel Georges <sam@daftspunk.com>
2017-06-22 08:52:42 +1000

    Apply defined constraints to deferred bindings

--- src/Database/Relations/DeferOneOrMany.php
+++ src/Database/Relations/DeferOneOrMany.php
@@ -46,6 +46,8 @@ trait DeferOneOrMany
                     $this->query = $query;
                     $this->addConstraints();
                 }
+
+                $this->addDefinedConstraintsToQuery($this);
             }
 
             /*
