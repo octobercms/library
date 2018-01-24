89832a16302f7c192dc198f3edee124bce22791e
Samuel Georges <sam@daftspunk.com>
2018-01-24 18:08:58 +1100

    Proxy ConsoleSupportServiceProvider properly Refs https://github.com/octobercms/october/pull/3353 Refs https://github.com/octobercms/october/issues/3321 Refs https://github.com/octobercms/october/pull/3328 Refs https://github.com/octobercms/october/issues/3359

---
+++ src/Foundation/Providers/ConsoleSupportServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace October\Rain\Foundation\Providers;
+
+use Illuminate\Support\AggregateServiceProvider;
+use Illuminate\Database\MigrationServiceProvider;
+use Illuminate\Foundation\Providers\ComposerServiceProvider;
+
+class ConsoleSupportServiceProvider extends AggregateServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * The provider class names.
+     *
+     * @var array
+     */
+    protected $providers = [
+        ArtisanServiceProvider::class,
+        MigrationServiceProvider::class,
+        ComposerServiceProvider::class,
+    ];
+}
