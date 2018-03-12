995b994b78a2df7ab072a9ce13d301a91370819f
JoakimBo <10329253+JoakimBo@users.noreply.github.com>
2018-03-12 20:18:44 +0100

    Added sluggable

--- src/Database/Behaviors/Sluggable.php
+++ src/Database/Behaviors/Sluggable.php
@@ -11,4 +11,28 @@ use \October\Rain\Database\ModelTraitBehavior;
 class Sluggable extends ModelTraitBehavior
 {
     use \October\Rain\Database\Traits\Sluggable;
+
+    public static function bootSluggable()
+    {
+        if (!$this->model->propertyExists('slugs'))
+        {
+            throw new Exception(sprintf(
+                'You must define a $slugs property in %s to use the Sluggable trait.', get_class($this->model)
+            ));
+        }
+        /*
+         * Set slugged attributes on new records
+         */
+        $model = $this->model;
+
+        $model->bindEvent('model.saveInternal', function() use ($model)
+        {
+            if ($model->exists)
+            {
+                return;
+            }
+
+            $model->slugAttributes();
+        });
+    }
 }
