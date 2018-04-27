db18a8db9ff8d06ba55ea6d38348f9233f06379f
Luke Towers <luke@luketowers.ca>
2018-04-26 19:03:22 -0600

    Support first level symlinks in File::localToPublic
    Example use case:
    If you are developing in October and have your /plugins folder symlinked to a separate folder on your computer then this will be able to correctly detect that it can convert /realpath/to/plugins/myauthor/myplugin/assets/myasset.js to /plugins/myauthor/myplugin/assets/myasset.js where as it didn't previously because it was looking at the realpaths of public_path vs the provided path without accounting for the potential for symlinks to exist within public_path.

--- src/Filesystem/Filesystem.php
+++ src/Filesystem/Filesystem.php
@@ -93,6 +93,17 @@ class Filesystem extends FilesystemBase
 
         if (strpos($path, $publicPath) === 0) {
             $result = str_replace("\\", "/", substr($path, strlen($publicPath)));
+        } else {
+            // Attempt to support first level symlinks
+            if ($directories = self::glob($publicPath . '/*', GLOB_NOSORT | GLOB_ONLYDIR)) {
+                foreach ($directories as $dir) {
+                    if (is_link($dir) && strpos($path, readlink($dir)) === 0) {
+                        // Get the path of the requested path relative to the symlink in the public path
+                        $relativeLinkedPath = substr($path, strlen(readlink($dir)));
+                        return str_replace("\\", "/", substr($dir, strlen($publicPath)) . $relativeLinkedPath);
+                    }
+                }
+            }
         }
 
         return $result;
@@ -159,7 +170,7 @@ class Filesystem extends FilesystemBase
     }
 
     /**
-     * Converts a path using path symbol. Returns the original path if 
+     * Converts a path using path symbol. Returns the original path if
      * no symbol is used and no default is specified.
      * @param  string $path
      * @param  mixed $default
