9f4f060a627e7f1b3bc59e694df02423c0e3d302
Kanji Furuhashi <pikanji@gmail.com>
2018-07-13 14:36:38 +0900

    Add EncryptCookies that reads config for cookies not to be encrypted

--- src/Foundation/Http/Kernel.php
+++ src/Foundation/Http/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \October\Rain\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
