03c53d308d85f82abee28dda99fd98fe63ca4651
pikanji <pikanji@gmail.com>
2018-07-20 11:40:41 +0900

    Enable use of unencrypted cookies (#335)
    Enables the use of the `cookie.unencryptedCookies` config item to specify cookies that should not be passed through the cookie encryption middleware. Cookie configuration file added in https://github.com/octobercms/october/commit/ab9dcaf09c0ee40cbc0a2eae8a0555da0c01cfa0, documented in octobercms/docs#3644. Credit to @pikanji

---
+++ src/Cookie/Middleware/EncryptCookies.php
@@ -0,0 +1,14 @@
+<?php namespace October\Rain\Cookie\Middleware;
+
+use Config;
+use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
+
+class EncryptCookies extends \Illuminate\Cookie\Middleware\EncryptCookies
+{
+    public function __construct(EncrypterContract $encrypter)
+    {
+        parent::__construct($encrypter);
+        $except = Config::get('cookie.unencryptedCookies', []);
+        $this->disableFor($except);
+    }
+}
