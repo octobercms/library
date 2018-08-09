43292a2e004623ffd7ca958de61f4ac79ac613bf
Luke Towers <luke@luketowers.ca>
2018-08-09 08:58:55 -0600

    Re-enable serialization of cookies
    Fixes: https://github.com/octobercms/october/issues/3681. Reference: https://laravel.com/docs/5.5/upgrade#upgrade-5.5.42, https://github.com/laravel/framework/compare/a2c54e6e69035111fa2709f9cdac55be811feb6b...c5dc9c870a8a1af6c7e827dd81bd15f0a1561cc4, Documentation added: https://github.com/octobercms/docs/commit/98cbcd15cd94dcb1c209ee7ed8e09952cfabd02c

--- src/Cookie/Middleware/EncryptCookies.php
+++ src/Cookie/Middleware/EncryptCookies.php
@@ -5,6 +5,13 @@ use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
 
 class EncryptCookies extends \Illuminate\Cookie\Middleware\EncryptCookies
 {
+    /**
+     * Indicates if cookies should be serialized.
+     *
+     * @var bool
+     */
+    protected static $serialize = true;
+
     public function __construct(EncrypterContract $encrypter)
     {
         parent::__construct($encrypter);
