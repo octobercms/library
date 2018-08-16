c643d02957f5e2bdbccb796e5d03a0ae3342c2b1
Luke Towers <luke@luketowers.ca>
2018-08-15 22:48:23 -0600

    Added missing model event inline documentation:
    model.beforeRestore
    model.afterRestore
    model.beforeValidate
    model.afterValidate

--- src/Database/Traits/Validation.php
+++ src/Database/Traits/Validation.php
@@ -143,6 +143,18 @@ trait Validation
             ? $this->throwOnValidation
             : true;
 
+        /**
+         * @event model.beforeValidate
+         * Called before the model is validated
+         *
+         * Example usage:
+         *
+         *     $model->bindEvent('model.beforeValidate', function () use (\October\Rain\Database\Model $model) {
+         *         // Prevent anything from validating ever!
+         *         return false;
+         *     });
+         *
+         */
         if (($this->fireModelEvent('validating') === false) || ($this->fireEvent('model.beforeValidate') === false)) {
             if ($throwOnValidation) {
                 throw new ModelException($this);
@@ -279,6 +291,17 @@ trait Validation
             }
         }
 
+        /**
+         * @event model.afterValidate
+         * Called after the model is validated
+         *
+         * Example usage:
+         *
+         *     $model->bindEvent('model.afterValidate', function () use (\October\Rain\Database\Model $model) {
+         *         \Log::info("{$model->name} successfully passed validation");
+         *     });
+         *
+         */
         $this->fireModelEvent('validated', false);
         $this->fireEvent('model.afterValidate');
 
