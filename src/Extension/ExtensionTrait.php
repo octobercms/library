f974699c4ff75da94e610e0a05edfdbf9400e1f0
SeriousKen <ken@seriousbrewing.co.uk>
2018-08-24 11:38:56 +0100

    Swapped hidden method order

--- src/Extension/ExtensionTrait.php
+++ src/Extension/ExtensionTrait.php
@@ -26,7 +26,7 @@ trait ExtensionTrait
 
     protected $extensionHidden = [
         'fields' => [],
-        'methods' => ['extensionIsHiddenMethod', 'extensionIsHiddenField']
+        'methods' => ['extensionIsHiddenField', 'extensionIsHiddenMethod']
     ];
 
     public function extensionApplyInitCallbacks()
