7abec307dbcf48022a2a5255edd98edc2e5cc1ff
cedric frossard <adrion.dev@gmail.com>
2019-03-14 15:08:08 +0100

    Added more unit tests

--- tests/Database/Traits/SluggableTest.php
+++ tests/Database/Traits/SluggableTest.php
@@ -17,60 +17,79 @@ class SluggableTest extends TestCase
         $capsule->bootEloquent();
 
         $capsule->schema()->create('testSoftDelete', function ($table) {
-               $table->increments('id');
-               $table->string('name');
-               $table->string('slug')->unique();
-               $table->softDeletes();
-               $table->timestamps();
-           });
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+        $capsule->schema()->create('test', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
     }
 
     public function testSlugGeneration()
     {
         /*
-         * Basic usage of slug Generator
-         */
-        $testModel1 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        * Basic usage of slug Generator
+        */
+        $testSoftModel1 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $this->assertEquals($testSoftModel1->slug, 'test');
+
+        $testSoftModel2 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $this->assertEquals($testSoftModel2->slug, 'test-2');
+
+        $testModel1 = TestModelSluggable::Create(['name' => 'test']);
         $this->assertEquals($testModel1->slug, 'test');
 
-        $testModel2 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $testModel2 = TestModelSluggable::Create(['name' => 'test']);
         $this->assertEquals($testModel2->slug, 'test-2');
     }
 
     public function testSlugGenerationWithSoftDelete()
     {
         /*
-         * Slug Generation when identical key is softDeleted
-         */
-        $testModel1 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
-        $this->assertEquals($testModel1->slug, 'test');
+        * Slug Generation when identical key is softDeleted
+        */
+        $testSoftModel1 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $this->assertEquals($testSoftModel1->slug, 'test');
 
-        $testModel1->delete();
-        $this->assertNotNull($testModel1->deleted_at);
+        $testSoftModel1->delete();
+        $this->assertNotNull($testSoftModel1->deleted_at);
 
-        $testModel2 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
-        $this->assertEquals($testModel2->slug, 'test-2');
+        $testSoftModel2 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $this->assertEquals($testSoftModel2->slug, 'test-2');
     }
 
     public function testSlugGenerationWithHardDelete()
     {
         /*
-         * Slug Generation when identical key was hardDeleted
-         */
-        $testModel1 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        * Slug Generation when identical key was hardDeleted
+        */
+        $testSoftModel1 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $this->assertEquals($testSoftModel1->slug, 'test');
+
+        $testSoftModel1->forceDelete();
+
+        $testSoftModel2 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $this->assertEquals($testSoftModel2->slug, 'test');
+
+        $testModel1 = TestModelSluggable::Create(['name' => 'test']);
         $this->assertEquals($testModel1->slug, 'test');
 
-        $testModel1->forceDelete();
+        $testModel1->delete();
 
-        $testModel2 = TestModelSluggableSoftDelete::Create(['name' => 'test']);
+        $testModel2 = TestModelSluggable::Create(['name' => 'test']);
         $this->assertEquals($testModel2->slug, 'test');
-
     }
 }
 
 /*
- * Class with Sluggable and SoftDelete traits
- */
+* Class with Sluggable and SoftDelete traits
+*/
 class TestModelSluggableSoftDelete extends \October\Rain\Database\Model
 {
     use \October\Rain\Database\Traits\SoftDelete;
@@ -80,5 +99,19 @@ class TestModelSluggableSoftDelete extends \October\Rain\Database\Model
     protected $fillable = ['name'];
     protected $dates = ['deleted_at'];
     protected $table = 'testSoftDelete';
+
+}
+
+/*
+* Class with only Sluggable trait
+*/
+class TestModelSluggable extends \October\Rain\Database\Model
+{
+    use \October\Rain\Database\Traits\Sluggable;
+
+    protected $slugs = ['slug' => 'name'];
+    protected $fillable = ['name'];
+    protected $dates = ['deleted_at'];
+    protected $table = 'test';
 
 }
