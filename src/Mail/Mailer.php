octobercms/library

commit f409ad425e68e38adf8917f80769ea6af828f267

Fix mail sending

src/Mail/Mailer.php
@@ -3,6 +3,7 @@
 use Event;
 use Config;
 use Illuminate\Mail\Mailer as MailerBase;
+use Illuminate\Contracts\Mail\Mailable as MailableContract;
 
 /**
  * Mailer class for sending mail.
@@ -39,19 +40,28 @@ class Mailer extends MailerBase
             return;
         }
 
+        if ($view instanceof MailableContract) {
+            return $this->sendMailable($view);
+        }
+
         /*
          * Inherit logic from Illuminate\Mail\Mailer
          */
         list($view, $plain, $raw) = $this->parseView($view);
 
         $data['message'] = $message = $this->createMessage();
-        $this->callMessageBuilder($callback, $message);
 
         if (is_bool($raw) && $raw === true) {
             $this->addContentRaw($message, $view, $plain);
         }
         else {
             $this->addContent($message, $view, $plain, $raw, $data);
+        }
+
+        call_user_func($callback, $message);
+
+        if (isset($this->to['address'])) {
+            $this->setGlobalTo($message);
         }
 
         /*
@@ -70,16 +80,14 @@ class Mailer extends MailerBase
         /*
          * Send the message
          */
-        $_message = $message->getSwiftMessage();
-        $response = $this->sendSwiftMessage($_message);
+        $this->sendSwiftMessage($message->getSwiftMessage());
+        $this->dispatchSentEvent($message);
 
         /*
          * Extensibility
          */
-        $this->fireEvent('mailer.send', [$view, $message, $response]);
-        Event::fire('mailer.send', [$this, $view, $message, $response]);
-
-        return $response;
+        $this->fireEvent('mailer.send', [$view, $message]);
+        Event::fire('mailer.send', [$this, $view, $message]);
     }
 
     /**

commit 819981708b8f89e2cef818905deb85fca4d630b4

Make dynamic subjects available to mail templates
Accomplishes this by firing the message callback before content is added to the message.

This fixes an issue where subjects set on the message object through the callback were not able to override a mail template's subject as they're supposed to in System\Classes\MailManager because the callback was called after the MailManager's listener for mailer.beforeAddContent had already fired. The original issue was reported and fixed in octobercms/october#1665. However, that fix was then later broken by https://github.com/octobercms/library/commit/f409ad425e68e38adf8917f80769ea6af828f267 which was for the L5.5 upgrade in Build 420. It's also worth noting that Laravel has moved the callback to be before the content is added starting in L5.6, so this change also brings us closer in line with Laravel for the future 6.0 upgrade (see https://github.com/laravel/framework/pull/22995 for that commit and background information).

src/Mail/Mailer.php
@@ -66,6 +66,10 @@ class Mailer extends MailerBase
 
         $data['message'] = $message = $this->createMessage();
 
+        if ($callback !== null) {
+            call_user_func($callback, $message);
+        }
+
         if (is_bool($raw) && $raw === true) {
             $this->addContentRaw($message, $view, $plain);
         }
@@ -73,10 +77,6 @@ class Mailer extends MailerBase
             $this->addContent($message, $view, $plain, $raw, $data);
         }
 
-        if ($callback !== null) {
-            call_user_func($callback, $message);
-        }
-        
         if (isset($this->to['address'])) {
             $this->setGlobalTo($message);
         }