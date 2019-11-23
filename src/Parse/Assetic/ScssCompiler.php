83797cafd4a8433b7f447e45574b2083a9a45f6a
Ben Thomson <git@alfreido.com>
2019-11-23 21:45:52 +0800

    Use correct Assetic class for ScssCompiler

--- src/Parse/Assetic/ScssCompiler.php
+++ src/Parse/Assetic/ScssCompiler.php
@@ -3,7 +3,7 @@
 use Event;
 use Assetic\Contracts\Asset\AssetInterface;
 use Assetic\Factory\AssetFactory;
-use Assetic\Contracts\Filter\ScssphpFilter;
+use Assetic\Filter\ScssFilter;
 use Assetic\Contracts\Filter\HashableInterface;
 use Assetic\Contracts\Filter\DependencyExtractorInterface;
 
@@ -14,7 +14,7 @@ use Assetic\Contracts\Filter\DependencyExtractorInterface;
  * @package october/parse
  * @author Alexey Bobkov, Samuel Georges
  */
-class ScssCompiler extends ScssphpFilter implements HashableInterface, DependencyExtractorInterface
+class ScssCompiler extends ScssFilter implements HashableInterface, DependencyExtractorInterface
 {
     protected $currentFiles = [];
 
