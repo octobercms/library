2589576405f88ccdf129aa081b31afc898e3eaf0
flynsarmy <flynsarmy@gmail.com>
2020-02-18 14:19:51 +1000

    Add tests

---
+++ tests/Database/Concerns/HasRelationshipsTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use October\Rain\Database\Model;
+
+class HasRelationshipsTest extends TestCase
+{
+    public function testGetRelationTypeDefinitions()
+    {
+        $model = new TestModelBelongsTo();
+        $this->assertEquals([], $model->getRelationTypeDefinitions('belongsToMany'));
+        $this->assertEquals(['relatedModel' => 'TestModelNoRelation'], $model->getRelationTypeDefinitions('belongsTo'));
+    }
+
+    public function testDynamicGetRelationTypeDefinitions()
+    {
+        TestModelBelongsTo::extend(function ($model) {
+            $model->belongsTo['secondRelatedModel'] = 'TestModelNoRelation';
+        });
+        $model = new TestModelBelongsTo();
+        $this->assertEquals([
+            'relatedModel' => 'TestModelNoRelation', 
+            'secondRelatedModel' => 'TestModelNoRelation'
+        ], $model->getRelationTypeDefinitions('belongsTo'));
+    }
+
+    public function testGetRelationTypeDefinition()
+    {
+        $model = new TestModelBelongsTo();
+        $this->assertEquals(null, $model->getRelationTypeDefinition('belongsTo', 'nonExistantRelation'));
+        $this->assertEquals('TestModelNoRelation', $model->getRelationTypeDefinition('belongsTo', 'relatedModel'));
+        $this->assertEquals(null, $model->getRelationTypeDefinition('belongsToMany', 'nonExistantRelation'));
+        $this->assertEquals(null, $model->getRelationTypeDefinition('belongsToMany', 'relatedModel'));
+    }
+
+    public function testDynamicGetRelationTypeDefinition()
+    {
+        TestModelBelongsTo::extend(function ($model) {
+            $model->belongsTo['secondRelatedModel'] = 'TestModelNoRelation';
+        });
+        $model = new TestModelBelongsTo();
+        $this->assertEquals('TestModelNoRelation', $model->getRelationTypeDefinition('belongsTo', 'secondRelatedModel'));
+    }
+}
+
+/*
+ * Class with implementation in the class itself
+ */
+class TestModelBelongsTo extends Model
+{
+    public $belongsTo = [
+        'relatedModel' => 'TestModelNoRelation'
+    ];
+    
+    public $purgeable = [];
+}
+
+/*
+ * Class with implementation in the class itself but without property
+ */
+class TestModelNoRelation extends Model
+{
+    public $implement = [
+        'October.Rain.Database.Behaviors.Purgeable'
+    ];
+}
