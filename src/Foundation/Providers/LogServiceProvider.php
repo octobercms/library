bfa824345c9e6b809c8a4557cb66a804ea986ab6
Samuel Georges <sam@daftspunk.com>
2020-02-27 19:03:44 +1100

    Configure default logger
    This accounts for application instances that do not have the logger.php configuration file, these will fall back to the default of using the single file logger

---
+++ src/Foundation/Providers/LogServiceProvider.php
@@ -0,0 +1,49 @@
+<?php namespace October\Rain\Foundation\Providers;
+
+use Illuminate\Log\LogServiceProvider as LogServiceProviderBase;
+
+class LogServiceProvider extends LogServiceProviderBase
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        parent::register();
+
+        /*
+         * After registration
+         */
+        $this->app->booting(function() {
+            $this->configureDefaultLogger();
+        });
+    }
+
+    /**
+     * Configure the default log channel for the application
+     * when no configuration is supplied.
+     *
+     * @return void
+     */
+    protected function configureDefaultLogger()
+    {
+        $config = $this->app->make('config');
+
+        if ($config->get('logging.default', null) !== null) {
+            return;
+        }
+
+        /*
+         * Set default values as single log file
+         */
+        $config->set('logging.default', 'single');
+
+        $config->set('logging.channels.single', [
+            'driver' => 'single',
+            'path' => storage_path('logs/system.log'),
+            'level' => 'debug',
+        ]);
+    }
+}
