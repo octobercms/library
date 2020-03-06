9762ac4be0dad2784671926256e2aa0debaef30e
Jacob de Keizer <jacob@vdlp.nl>
2020-03-06 17:09:10 +0100

    Add static method typehint to schema facade

--- src/Support/Facades/Schema.php
+++ src/Support/Facades/Schema.php
@@ -2,13 +2,27 @@
 
 use October\Rain\Support\Facade;
 
+/**
+ * @method static \Illuminate\Database\Schema\Builder create(string $table, \Closure $callback)
+ * @method static \Illuminate\Database\Schema\Builder drop(string $table)
+ * @method static \Illuminate\Database\Schema\Builder dropIfExists(string $table)
+ * @method static \Illuminate\Database\Schema\Builder table(string $table, \Closure $callback)
+ * @method static \Illuminate\Database\Schema\Builder rename(string $from, string $to)
+ * @method static void defaultStringLength(int $length)
+ * @method static bool hasTable(string $table)
+ * @method static bool hasColumn(string $table, string $column)
+ * @method static bool hasColumns(string $table, array $columns)
+ * @method static \Illuminate\Database\Schema\Builder disableForeignKeyConstraints()
+ * @method static \Illuminate\Database\Schema\Builder enableForeignKeyConstraints()
+ * @method static void registerCustomDoctrineType(string $class, string $name, string $type)
+ */
 class Schema extends Facade
 {
     /**
      * Get a schema builder instance for a connection.
      *
      * @param  string  $name
-     * @return \October\Rain\Database\Schema\Builder
+     * @return \October\Rain\Database\Schema\Blueprint
      */
     public static function connection($name)
     {
@@ -22,7 +36,7 @@ class Schema extends Facade
     /**
      * Get a schema builder instance for the default connection.
      *
-     * @return \October\Rain\Database\Schema\Builder
+     * @return \October\Rain\Database\Schema
      */
     protected static function getFacadeAccessor()
     {
