c8ef2a340367046fc870973dbc8d4f9fa422cbdd
jacobdekeizer <jacobdekeizer@gmail.com>
2020-05-04 08:44:04 +0200

    Update src/Support/Facades/Config.php
    Co-authored-by: Ben Thomson <ben@abweb.com.au>

--- src/Support/Facades/Config.php
+++ src/Support/Facades/Config.php
@@ -10,6 +10,20 @@ use October\Rain\Support\Facade;
  * @method static void set(array|string $key, $value)
  * @method static void prepend(string $key, $value)
  * @method static void push(string $key, $value)
+ * @method static array parseConfigKey(string $key)
+ * @method static void package(string $namespace, string $hint)
+ * @method static void afterLoading(string $namespace, \Closure $callback)
+ * @method static void addNamespace(string $namespace, string $hint)
+ * @method static array getNamespaces()
+ * @method static \October\Rain\Config\LoaderInterface getLoader()
+ * @method static void setLoader(\October\Rain\Config\LoaderInterface $loader)
+ * @method static string getEnvironment()
+ * @method static array getAfterLoadCallbacks()
+ * @method static array getItems()
+ * @method static bool offsetExists(string $key)
+ * @method static mixed offsetGet(string $key)
+ * @method static void offsetSet(string $key, mixed $value)
+ * @method static void offsetUnset(string $key)
  *
  * @see \October\Rain\Config\Repository
  */
