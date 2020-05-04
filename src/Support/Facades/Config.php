1d366ab303819373e29d1ddde8a2c4d01585ad13
jacobdekeizer <jacobdekeizer@gmail.com>
2020-05-04 09:24:08 +0200

    Add static method typehints to schema facade (#464)

--- src/Support/Facades/Config.php
+++ src/Support/Facades/Config.php
@@ -2,13 +2,35 @@
 
 use October\Rain\Support\Facade;
 
+/**
+ * @method static bool has(string $key)
+ * @method static bool hasGroup(string $key)
+ * @method static mixed get(array|string $key, $default = null)
+ * @method static array all()
+ * @method static void set(array|string $key, $value)
+ * @method static void prepend(string $key, $value)
+ * @method static void push(string $key, $value)
+ * @method static array parseConfigKey(string $key)
+ * @method static void package(string $namespace, string $hint)
+ * @method static void afterLoading(string $namespace, \Closure $callback)
+ * @method static void addNamespace(string $namespace, string $hint)
+ * @method static array getNamespaces()
+ * @method static \October\Rain\Config\LoaderInterface getLoader()
+ * @method static void setLoader(\October\Rain\Config\LoaderInterface $loader)
+ * @method static string getEnvironment()
+ * @method static array getAfterLoadCallbacks()
+ * @method static array getItems()
+ * @method static bool offsetExists(string $key)
+ * @method static mixed offsetGet(string $key)
+ * @method static void offsetSet(string $key, mixed $value)
+ * @method static void offsetUnset(string $key)
+ *
+ * @see \October\Rain\Config\Repository
+ */
 class Config extends Facade
 {
     /**
      * Get the registered name of the component.
-     *
-     * Resolves to:
-     * - October\Rain\Config\Repository
      *
      * @return string
      */
--- src/Support/Facades/Flash.php
+++ src/Support/Facades/Flash.php
@@ -2,13 +2,25 @@
 
 use October\Rain\Support\Facade;
 
+/**
+ * @method static bool check()
+ * @method static array all(string $format = null)
+ * @method static array get(string $key, string $format = null)
+ * @method static array|\October\Rain\Flash\FlashBag error(string $message = null)
+ * @method static array|\October\Rain\Flash\FlashBag success(string $message = null)
+ * @method static array|\October\Rain\Flash\FlashBag warning(string $message = null)
+ * @method static array|\October\Rain\Flash\FlashBag info(string $message = null)
+ * @method static \October\Rain\Flash\FlashBag add(string $key, string $message)
+ * @method static void store()
+ * @method static void forget(string $key = null)
+ * @method static void purge();
+ *
+ * @see \October\Rain\Flash\FlashBag
+ */
 class Flash extends Facade
 {
     /**
      * Get the registered name of the component.
-     *
-     * Resolves to:
-     * - October\Rain\Support\FlashBag
      *
      * @return string
      */
