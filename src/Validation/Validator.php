087781632b5c0fa3b3646e98bddd172abd0f15e5
Ben Thomson <git@alfreido.com>
2020-06-04 17:27:47 +0800

    Code smell fix

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Validation\Validator as ValidatorContract;
  *
  * The only difference between this and the BaseValidator is that it resets the email validation rule to use the
  * `filter` method by default.
- * 
+ *
  * It also does some custom handling of Rule objects through the FormatsMessages concern.
  */
 class Validator extends BaseValidator implements ValidatorContract
