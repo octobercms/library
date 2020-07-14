8e17e36ab5dfb8551e6f16bc815c385c35de6dd8
Romain 'Maz' BILLOIR <romainb@octobercms.fr>
2020-07-14 18:22:38 +0200

    string value should not be trimmed

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -1204,13 +1204,6 @@ class Model extends EloquentModel
             $value = json_encode($value);
         }
 
-        /*
-         * Trim strings
-         */
-        if (is_string($value)) {
-            $value = trim($value);
-        }
-
         $result = parent::setAttribute($key, $value);
 
         /**
