81403c54f68067dd27be275808da1dc1e716e671
RomainMazB <romainmazb@gmail.com>
2020-07-24 23:24:50 +0200

    add missing attachOne keyType detection

--- src/Database/Concerns/HasRelationships.php
+++ src/Database/Concerns/HasRelationships.php
@@ -653,9 +653,14 @@ trait HasRelationships
 
         $table = $instance->getTable();
 
+        $keyType = data_get(
+            $instance->getRelationDefinitions(),
+            'morphTo.attachment.keyType'
+        );
+
         $localKey = $localKey ?: $this->getKeyName();
 
-        return new AttachOne($instance->newQuery(), $this, $table.'.'.$type, $table.'.'.$id, $isPublic, $localKey, $relationName);
+        return new AttachMany($instance->newQuery(), $this, $table.'.'.$type, $table.'.'.$id, $isPublic, $localKey, $relationName, $keyType);
     }
 
     /**
