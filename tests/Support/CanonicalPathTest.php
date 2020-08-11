db4d4ad50163de3f47c25169e8f65e0b3d8bd9f9
Ben Thomson <git@alfreido.com>
2020-08-11 15:35:09 +0800

    Include drive letter in assertions, if required

--- tests/Support/CanonicalPathTest.php
+++ tests/Support/CanonicalPathTest.php
@@ -9,6 +9,9 @@ class CanonicalPathTest extends TestCase
     public function testDirectPaths()
     {
         $dir = dirname(__DIR__) . '/fixtures/paths';
+        $drive = (preg_match('/^([A-Z]:)/', __FILE__, $matches) === 1)
+            ? $matches[1]
+            : null;
 
         $this->assertEquals(
             str_replace('/', DIRECTORY_SEPARATOR, $dir . '/dir1'),
@@ -75,18 +78,18 @@ class CanonicalPathTest extends TestCase
         );
 
         $this->assertEquals(
-            str_replace('/', DIRECTORY_SEPARATOR, '/tmp'),
+            str_replace('/', DIRECTORY_SEPARATOR, ($drive ?? '') . '/tmp'),
             resolve_path('/tmp/')
         );
         $this->assertEquals(
-            str_replace('/', DIRECTORY_SEPARATOR, '/tmp'),
+            str_replace('/', DIRECTORY_SEPARATOR, ($drive ?? '') . '/tmp'),
             realpath('/tmp/')
         );
 
         // Path /dev/null technically doesn't exist - we do return a result, but realpath() won't. This is a fringe case
         // that's unlikely to come up.
         $this->assertEquals(
-            str_replace('/', DIRECTORY_SEPARATOR, '/dev/null'),
+            str_replace('/', DIRECTORY_SEPARATOR, ($drive ?? '') . '/dev/null'),
             resolve_path('\\dev\\null')
         );
         $this->assertFalse(
