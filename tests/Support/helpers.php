957e94c3fb18a85b56aa33dba38920c1b0bbb08a
Marc Jauvin <marc.jauvin@gmail.com>
2020-09-23 22:41:20 -0400

    fix code quality warnings

--- tests/Support/helpers.php
+++ tests/Support/helpers.php
@@ -3,11 +3,10 @@
 use October\Rain\Support\helpers;
 use October\Rain\Foundation\Application;
 
-
 class HelpersTest extends TestCase
 {
     public function setUp()
-    {   
+    {
         parent::setUp();
 
         $this->app = new Application();
