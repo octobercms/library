55fff80fe896f99fdcc0028d3535091861ea24cd
Marc Jauvin <marc.jauvin@gmail.com>
2020-10-20 08:32:01 -0400

    use PathResolver::standardize()

--- tests/Support/HelpersTest.php
+++ tests/Support/HelpersTest.php
@@ -35,7 +35,7 @@ class HelpersTest extends TestCase
     public function testUploadsPath()
     {
         $expected = Config::get('cms.storage.uploads.path', app('path.uploads'));
-        $expected = str_replace('/', DIRECTORY_SEPARATOR, $expected);
+        $expected = PathResolver::standardize($expected);
 
         $this->assertEquals($expected, uploads_path());
         $this->assertEquals(PathResolver::join($expected, '/extra'), uploads_path('/extra'));
@@ -44,7 +44,7 @@ class HelpersTest extends TestCase
     public function testMediaPath()
     {
         $expected = Config::get('cms.storage.media.path', app('path.media'));
-        $expected = str_replace('/', DIRECTORY_SEPARATOR, $expected);
+        $expected = PathResolver::standardize($expected);
 
         $this->assertEquals($expected, media_path());
         $this->assertEquals(PathResolver::join($expected, '/extra'), media_path('/extra'));
