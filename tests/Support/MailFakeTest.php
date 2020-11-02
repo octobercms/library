acc251804bcbe0e1eed539070756cbd2c6a8f1bf
Marc Jauvin <marc.jauvin@gmail.com>
2020-11-02 14:29:55 -0500

    add unit tests for MailFake

---
+++ tests/Support/MailFakeTest.php
@@ -0,0 +1,48 @@
+<?php
+
+class MailFakeTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->mailer = new \October\Rain\Support\Testing\Fakes\MailFake();
+        $this->view = 'mail-test-view';
+        $this->recipient = 'fake@localhost';
+        $this->subject = 'MailFake test';
+    }
+
+    public function testSend()
+    {
+        $this->mailer->send($this->view, [], function ($mailer) {
+            $mailer->to($this->recipient);
+            $mailer->subject('MailFake test');
+        });
+
+        $this->mailer->assertSent($this->view, 1);
+
+        $this->mailer->assertSent($this->view, function ($mailer) {
+            return $mailer->hasTo($this->recipient);
+        });
+
+        $this->mailer->assertSent($this->view, function ($mailer) {
+            return $mailer->subject === $this->subject;
+        });
+    }
+
+    public function testQueue()
+    {
+        $this->mailer->queue($this->view, [], function ($mailer) {
+            $mailer->to($this->recipient);
+            $mailer->subject($this->subject);
+        });
+
+        $this->mailer->assertQueued($this->view, 1);
+
+        $this->mailer->assertQueued($this->view, function ($mailer) {
+            return $mailer->hasTo($this->recipient);
+        });
+
+        $this->mailer->assertSent($this->view, function ($mailer) {
+            return $mailer->subject === $this->subject;
+        });
+    }
+}
