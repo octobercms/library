b57093b473f597051b5f16be1efe39e5eabd3df4
Luke Towers <github@luketowers.ca>
2020-12-20 14:25:17 -0600

    no need to subsplit theme and plugin anymore

--- build/rain-split-stable.sh
+++ build/rain-split-stable.sh
@@ -6,9 +6,9 @@ mkdir -p october
 pushd october
 ./../git-subsplit.sh init git@github.com:octobercms/october.git
 ./../git-subsplit.sh update
-./../git-subsplit.sh publish --heads="1.0" modules/backend:git@github.com:octoberrain/backend.git
-./../git-subsplit.sh publish --heads="1.0" modules/cms:git@github.com:octoberrain/cms.git
-./../git-subsplit.sh publish --heads="1.0" modules/system:git@github.com:octoberrain/system.git
-./../git-subsplit.sh publish --heads="1.0" themes/demo:git@github.com:octoberrain/demo-theme.git
-./../git-subsplit.sh publish --heads="1.0" plugins/october/demo:git@github.com:octoberrain/demo-plugin.git
+./../git-subsplit.sh publish --heads="1.1" modules/backend:git@github.com:octoberrain/backend.git
+./../git-subsplit.sh publish --heads="1.1" modules/cms:git@github.com:octoberrain/cms.git
+./../git-subsplit.sh publish --heads="1.1" modules/system:git@github.com:octoberrain/system.git
+# ./../git-subsplit.sh publish --heads="1.0" themes/demo:git@github.com:octoberrain/demo-theme.git
+# ./../git-subsplit.sh publish --heads="1.0" plugins/october/demo:git@github.com:octoberrain/demo-plugin.git
 popd
