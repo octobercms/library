d8e8d87a9fd6438f8bcbeda5bbc3285571d9bfdf
Samuel Georges <sam@daftspunk.com>
2021-03-09 18:10:27 +1100

    Bump less.php for php 8

--- build/rain-split-stable.sh
+++ build/rain-split-stable.sh
@@ -6,9 +6,9 @@ mkdir -p october
 pushd october
 ./../git-subsplit.sh init git@github.com:octobercms/october.git
 ./../git-subsplit.sh update
-./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.1" modules/backend:git@github.com:octoberrain/backend.git
-./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.1" modules/cms:git@github.com:octoberrain/cms.git
-./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.1" modules/system:git@github.com:octoberrain/system.git
-./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.1" themes/demo:git@github.com:octoberrain/demo-theme.git
-./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.1" plugins/october/demo:git@github.com:octoberrain/demo-plugin.git
+./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.2" modules/backend:git@github.com:octoberrain/backend.git
+./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.2" modules/cms:git@github.com:octoberrain/cms.git
+./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.2" modules/system:git@github.com:octoberrain/system.git
+./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.2" themes/demo:git@github.com:octoberrain/demo-theme.git
+./../git-subsplit.sh publish --heads="1.1" --tags="v1.1.2" plugins/october/demo:git@github.com:octoberrain/demo-plugin.git
 popd
