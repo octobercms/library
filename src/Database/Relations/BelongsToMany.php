a0d8a73cecbba82ec529174697670fa8c2a75055
Samuel Georges <sam@daftspunk.com>
2021-03-24 10:46:48 +1100

    Fixes foundation classes
    This restores the previous functionality broken by the previous team
    
    Refs https://github.com/octobercms/october/issues/5542

--- src/Database/Relations/BelongsToMany.php
+++ src/Database/Relations/BelongsToMany.php
@@ -142,10 +142,14 @@ class BelongsToMany extends BelongsToManyBase
             return;
         }
 
-        /**
-         * @see Illuminate\Database\Eloquent\Relations\Concerns\InteractsWithPivotTable
-         */
-        parent::attach($id, $attributes, $touch);
+        // Here we will insert the attachment records into the pivot table. Once we have
+        // inserted the records, we will touch the relationships if necessary and the
+        // function will return. We can parse the IDs before inserting the records.
+        $this->newPivotStatement()->insert($insertData);
+
+        if ($touch) {
+            $this->touchIfTouching();
+        }
 
         /**
          * @event model.relation.afterAttach
@@ -172,7 +176,7 @@ class BelongsToMany extends BelongsToManyBase
     {
         $attachedIdList = $this->parseIds($ids);
         if (empty($attachedIdList)) {
-            $attachedIdList = $this->allRelatedIds()->all();
+            $attachedIdList = $this->newPivotQuery()->lists($this->relatedPivotKey);
         }
 
         /**
--- src/Database/Relations/DefinedConstraints.php
+++ src/Database/Relations/DefinedConstraints.php
@@ -121,26 +121,4 @@ trait DefinedConstraints
             $query->$scope($this->parent);
         }
     }
-
-    /**
-     * Create a new query builder for the pivot table.
-     *
-     * This is an extension of Laravel's `newPivotQuery` method that allows `belongsToMany` and `morphToMany` relations
-     * to have conditions.
-     *
-     * @return \Illuminate\Database\Query\Builder
-     */
-    public function newPivotQuery()
-    {
-        $query = parent::newPivotQuery();
-
-        // add relation's conditions and scopes to the query
-        $this->addDefinedConstraintsToQuery($query);
-
-        $related = $this->getRelated();
-
-        return $query
-            ->join($related->getTable(), $related->getQualifiedKeyName(), '=', $this->getOtherKey())
-            ->select($this->getTable().'.*');
-    }
 }
