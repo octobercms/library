a6b53db05eeb2d695ea780290094fe4925760777
Tobias Kündig <tobias@offline.ch>
2021-04-09 07:31:31 +0200

    Make the stylesheet minifier aware of CSS vars (#566)
    Ignore comments inside parentheses and removed 0px -> 0 conversion

--- tests/Assetic/StylesheetMinifyTest.php
+++ tests/Assetic/StylesheetMinifyTest.php
@@ -2,14 +2,51 @@
 
 use October\Rain\Assetic\Filter\StylesheetMinify;
 
+include __DIR__ . '/MockAsset.php';
+
 class StylesheetMinifyTest extends TestCase
 {
-    public function testUnitRemoval()
-    {
-        include __DIR__ . '/MockAsset.php';
 
+    public function testSpaceRemoval()
+    {
         $input  = 'body {width: calc(99.9% * 1/1 - 0px); height: 0px;}';
-        $output = 'body {width:calc(99.9% * 1/1 - 0px);height:0}';
+        $output = 'body {width:calc(99.9% * 1/1 - 0px);height:0px}';
+
+        $mockAsset = new MockAsset($input);
+        $result    = new StylesheetMinify();
+        $result->filterDump($mockAsset);
+
+        $this->assertEquals($output, $mockAsset->getContent());
+    }
+
+    public function testCommentRemoval()
+    {
+        $input  = 'body {/* First comment */} /* Second comment */';
+        $output = 'body {}';
+
+        $mockAsset = new MockAsset($input);
+        $result    = new StylesheetMinify();
+        $result->filterDump($mockAsset);
+
+        $this->assertEquals($output, $mockAsset->getContent());
+    }
+
+    public function testCommentPreservationInVar()
+    {
+        $input  = '--ring-inset: var(--empty, /*!*/ /*!*/);';
+        $output = '--ring-inset:var(--empty,/*!*/ /*!*/);';
+
+        $mockAsset = new MockAsset($input);
+        $result    = new StylesheetMinify();
+        $result->filterDump($mockAsset);
+
+        $this->assertEquals($output, $mockAsset->getContent());
+    }
+
+    public function testUnitPreservationInVar()
+    {
+        $input  = '--offset-width: 0px';
+        $output = '--offset-width:0px';
 
         $mockAsset = new MockAsset($input);
         $result    = new StylesheetMinify();
