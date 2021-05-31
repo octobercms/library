98bbef99b96dc13b62361d94a88337aaa2b0c3a0
Samuel Georges <sam@daftspunk.com>
2021-05-31 13:03:25 +1000

    Update build scripts

--- build/rain-split-1.0.sh
+++ build/rain-split-1.0.sh
@@ -6,9 +6,9 @@ mkdir -p october
 pushd october
 ./../git-subsplit.sh init git@github.com:octobercms/october.git
 ./../git-subsplit.sh update
-./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.470" modules/backend:git@github.com:octoberrain/backend.git
-./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.470" modules/cms:git@github.com:octoberrain/cms.git
-./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.470" modules/system:git@github.com:octoberrain/system.git
-./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.470" themes/demo:git@github.com:octoberrain/demo-theme.git
-./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.470" plugins/october/demo:git@github.com:octoberrain/demo-plugin.git
+./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.473" modules/backend:git@github.com:octoberrain/backend.git
+./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.473" modules/cms:git@github.com:octoberrain/cms.git
+./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.473" modules/system:git@github.com:octoberrain/system.git
+./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.473" themes/demo:git@github.com:octoberrain/demo-theme.git
+./../git-subsplit.sh publish --heads="1.0" --tags="v1.0.473" plugins/october/demo:git@github.com:octoberrain/demo-plugin.git
 popd
