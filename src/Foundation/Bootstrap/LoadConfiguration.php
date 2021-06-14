d5c73f9df0916a1e351e66226e88042745525de2
Samuel Georges <sam@daftspunk.com>
2021-06-14 19:43:51 +1000

    Clean support for env specific env files

--- src/Foundation/Bootstrap/LoadConfiguration.php
+++ src/Foundation/Bootstrap/LoadConfiguration.php
@@ -23,7 +23,7 @@ class LoadConfiguration
             return $config->get('app.env', 'production');
         });
 
-        date_default_timezone_set($config['app.timezone']);
+        date_default_timezone_set($config->get('app.timezone', 'UTC'));
 
         mb_internal_encoding('UTF-8');
 
--- src/Foundation/Bootstrap/LoadEnvironmentVariables.php
+++ src/Foundation/Bootstrap/LoadEnvironmentVariables.php
@@ -1,70 +1,22 @@
 <?php namespace October\Rain\Foundation\Bootstrap;
 
-use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
-use Symfony\Component\Console\Input\ArgvInput;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables as LoadEnvironmentVariablesBase;
 
-class LoadEnvironmentVariables
+/**
+ * LoadEnvironmentVariables
+ */
+class LoadEnvironmentVariables extends LoadEnvironmentVariablesBase
 {
     /**
-     * Bootstrap the given application.
-     *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
-     * @return void
+     * bootstrap the given application
      */
     public function bootstrap(Application $app)
     {
-        $this->checkForSpecificEnvironmentFile($app);
-
-        try {
-            DotEnv::create($app->environmentPath(), $app->environmentFile())->load();
-        }
-        catch (InvalidPathException $e) {
-            //
-        }
+        parent::bootstrap($app);
 
         $app->detectEnvironment(function () {
             return env('APP_ENV', 'production');
         });
-    }
-
-    /**
-     * Detect if a custom environment file matching the APP_ENV exists.
-     *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
-     * @return void
-     */
-    protected function checkForSpecificEnvironmentFile($app)
-    {
-        if ($app->runningInConsole() && ($input = new ArgvInput)->hasParameterOption('--env')) {
-            $this->setEnvironmentFilePath(
-                $app,
-                $app->environmentFile().'.'.$input->getParameterOption('--env')
-            );
-        }
-
-        if (!env('APP_ENV')) {
-            return;
-        }
-
-        $this->setEnvironmentFilePath(
-            $app,
-            $app->environmentFile().'.'.env('APP_ENV')
-        );
-    }
-
-    /**
-     * Load a custom environment file.
-     *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
-     * @param  string  $file
-     * @return void
-     */
-    protected function setEnvironmentFilePath($app, $file)
-    {
-        if (file_exists($app->environmentPath().'/'.$file)) {
-            $app->loadEnvironmentFrom($file);
-        }
     }
 }
