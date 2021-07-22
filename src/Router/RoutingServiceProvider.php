ce3054744364a47b53b95c4a3d79e2a70cd2ac6b
Samuel Georges <sam@daftspunk.com>
2021-07-22 15:47:28 +1000

    Add url validation to router
    Refactor http_build_url implementation

--- src/Router/RoutingServiceProvider.php
+++ src/Router/RoutingServiceProvider.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Routing\RoutingServiceProvider as RoutingServiceProviderBase;
 
+/**
+ * RoutingServiceProvider
+ *
+ * @package october\router
+ * @author Alexey Bobkov, Samuel Georges
+ */
 class RoutingServiceProvider extends RoutingServiceProviderBase
 {
     /**
@@ -37,48 +43,6 @@ class RoutingServiceProvider extends RoutingServiceProviderBase
     {
         $this->app->singleton('router', function ($app) {
             return new CoreRouter($app['events'], $app);
-        });
-    }
-
-    /**
-     * Register the URL generator service.
-     *
-     * @return void
-     */
-    protected function registerUrlGenerator()
-    {
-        $this->app->singleton('url', function ($app) {
-            $routes = $app['router']->getRoutes();
-
-            // The URL generator needs the route collection that exists on the router.
-            // Keep in mind this is an object, so we're passing by references here
-            // and all the registered routes will be available to the generator.
-            $app->instance('routes', $routes);
-
-            $url = new UrlGenerator(
-                $routes,
-                $app->rebinding(
-                    'request',
-                    $this->requestRebinder()
-                )
-            );
-
-            $url->setSessionResolver(function () {
-                return $this->app['session'];
-            });
-
-            $url->setKeyResolver(function () {
-                return $this->app->make('config')->get('app.key');
-            });
-
-            // If the route collection is "rebound", for example, when the routes stay
-            // cached for the application, we will need to rebind the routes on the
-            // URL generator instance so it has the latest version of the routes.
-            $app->rebinding('routes', function ($app, $routes) {
-                $app['url']->setRoutes($routes);
-            });
-
-            return $url;
         });
     }
 }
--- src/Router/helpers.php
+++ src/Router/helpers.php
@@ -1,6 +1,34 @@
 <?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Jake A. Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-// PECL HTTP constant definitions
+/**
+ * URL constants as defined in the PHP Manual under "Constants usable with
+ * http_build_url()".
+ *
+ * @see http://us2.php.net/manual/en/http.constants.php#http.constants.url
+ */
 if (!defined('HTTP_URL_REPLACE')) {
     define('HTTP_URL_REPLACE', 1);
 }
@@ -36,18 +64,129 @@ if (!defined('HTTP_URL_STRIP_ALL')) {
 }
 
 if (!function_exists('http_build_url')) {
+
     /**
-     * Polyfill for `http_build_url` method provided by PECL HTTP extension.
+     * Build a URL.
      *
-     * @see \October\Rain\Router\UrlGenerator::buildUrl()
-     * @param array $url
-     * @param array $replace
-     * @param mixed $flags
-     * @param array $newUrl
+     * The parts of the second URL will be merged into the first according to
+     * the flags argument.
+     *
+     * @see https://github.com/jakeasmith/http_build_url
+     *
+     * @param mixed $url     (part(s) of) an URL in form of a string or
+     *                       associative array like parse_url() returns
+     * @param mixed $parts   same as the first argument
+     * @param int   $flags   a bitmask of binary or'ed HTTP_URL constants;
+     *                       HTTP_URL_REPLACE is the default
+     * @param array $new_url if set, it will be filled with the parts of the
+     *                       composed url like parse_url() would return
      * @return string
      */
-    function http_build_url(array $url, array $replace = [], $flags = HTTP_URL_REPLACE, array &$newUrl = [])
+    function http_build_url($url, $replace = [], $flags = HTTP_URL_REPLACE, &$newUrl = []): string
     {
-        return \October\Rain\Router\UrlGenerator::buildUrl($url, $replace, $flags, $newUrl);
+        if (is_string($url)) {
+            $url = parse_url($url);
+        }
+        if (is_string($replace)) {
+            $replace = parse_url($replace);
+        }
+
+        $urlSegments = ['scheme', 'host', 'user', 'pass', 'port', 'path', 'query', 'fragment'];
+
+        // Set flags - HTTP_URL_STRIP_ALL and HTTP_URL_STRIP_AUTH cover several other flags.
+        if ($flags & HTTP_URL_STRIP_ALL) {
+            $flags |= HTTP_URL_STRIP_USER
+                   | HTTP_URL_STRIP_PASS
+                   | HTTP_URL_STRIP_PORT
+                   | HTTP_URL_STRIP_PATH
+                   | HTTP_URL_STRIP_QUERY
+                   | HTTP_URL_STRIP_FRAGMENT;
+        } elseif ($flags & HTTP_URL_STRIP_AUTH) {
+            $flags |= HTTP_URL_STRIP_USER
+                   | HTTP_URL_STRIP_PASS;
+        }
+
+        // Filter $url and $replace arrays to strip out unknown segments
+        array_change_key_case($url, CASE_LOWER);
+        array_change_key_case($replace, CASE_LOWER);
+
+        $url = array_filter($url, function ($value, $key) use ($urlSegments) {
+            return (in_array($key, $urlSegments) && isset($value));
+        }, ARRAY_FILTER_USE_BOTH);
+        $replace = array_filter($replace, function ($value, $key) use ($urlSegments) {
+            return (in_array($key, $urlSegments) && isset($value));
+        }, ARRAY_FILTER_USE_BOTH);
+
+        // Replace URL parts if required
+        if ($flags & HTTP_URL_REPLACE) {
+            $url = array_replace($url, $replace);
+        } else {
+            // Process joined paths
+            if (($flags & HTTP_URL_JOIN_PATH) && isset($replace['path'])) {
+                $urlPath = (isset($url['path'])) ? explode('/', trim($url['path'], '/')) : [];
+                $joinedPath = explode('/', trim($replace['path'], '/'));
+
+                $url['path'] = '/' . implode('/', array_merge($urlPath, $joinedPath));
+            }
+
+            // Process joined query string
+            if (($flags & HTTP_URL_JOIN_QUERY) && isset($replace['query'])) {
+                $urlQuery = $joinedQuery = [];
+
+                parse_str($url['query'] ?? '', $urlQuery);
+                parse_str($replace['query'] ?? '', $joinedQuery);
+
+                $url['query'] = http_build_query(array_replace_recursive($urlQuery, $joinedQuery));
+            }
+        }
+
+        // Strip segments as necessary
+        foreach ($urlSegments as $segment) {
+            $strip = 'HTTP_URL_STRIP_' . strtoupper($segment);
+
+            if (!defined($strip)) {
+                continue;
+            }
+
+            if ($flags & constant($strip)) {
+                unset($url[$segment]);
+            }
+        }
+
+        // Make new URL available
+        $newUrl = $url;
+
+        // Generate URL string
+        $urlString = '';
+
+        if (!empty($url['scheme'])) {
+            $urlString .= $url['scheme'] . '://';
+        }
+        if (!empty($url['user'])) {
+            $urlString .= $url['user'];
+
+            if (!empty($url['pass'])) {
+                $urlString .= ':' . $url['pass'];
+            }
+
+            $urlString .= '@';
+        }
+        if (!empty($url['host'])) {
+            $urlString .= $url['host'];
+        }
+        if (!empty($url['port'])) {
+            $urlString .= ':' . $url['port'];
+        }
+        if (!empty($url['path'])) {
+            $urlString .= ((substr($url['path'], 0, 1) !== '/') ? '/' : '') . $url['path'];
+        }
+        if (!empty($url['query'])) {
+            $urlString .= '?' . $url['query'];
+        }
+        if (!empty($url['fragment'])) {
+            $urlString .= '#' . $url['fragment'];
+        }
+
+        return $urlString;
     }
 }
