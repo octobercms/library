5692fe6b4370c2c933c9eec6a2d3b34a646d49c9
Samuel Georges <sam@daftspunk.com>
2021-08-18 17:43:00 +1000

    Simple accessor for HasManyThrough
    There is no simple setter since this is a read-only relation type

--- src/Database/Relations/HasManyThrough.php
+++ src/Database/Relations/HasManyThrough.php
@@ -4,18 +4,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough as HasManyThroughBase;
 
+/**
+ * HasManyThrough class extension
+ */
 class HasManyThrough extends HasManyThroughBase
 {
     use DefinedConstraints;
 
     /**
-     * @var string The "name" of the relationship.
+     * @var string relationName
      */
     protected $relationName;
 
     /**
-     * Create a new has many relationship instance.
-     * @return void
+     * __construct a new has many relationship instance.
      */
     public function __construct(Builder $query, Model $farParent, Model $parent, $firstKey, $secondKey, $localKey, $secondLocalKey, $relationName = null)
     {
@@ -27,15 +29,30 @@ class HasManyThrough extends HasManyThroughBase
     }
 
     /**
-     * Determine whether close parent of the relation uses Soft Deletes.
-     *
+     * parentSoftDeletes determines whether close parent of the relation uses Soft Deletes.
      * @return bool
      */
     public function parentSoftDeletes()
     {
         $uses = class_uses_recursive(get_class($this->parent));
 
-        return in_array('October\Rain\Database\Traits\SoftDelete', $uses) ||
-            in_array('Illuminate\Database\Eloquent\SoftDeletes', $uses);
+        return in_array(\October\Rain\Database\Traits\SoftDelete::class, $uses) ||
+            in_array(\Illuminate\Database\Eloquent\SoftDeletes::class, $uses);
+    }
+
+    /**
+     * getSimpleValue is a helper for getting this relationship simple value,
+     * generally useful with form values.
+     */
+    public function getSimpleValue()
+    {
+        $value = null;
+        $relationName = $this->relationName;
+
+        if ($relation = $this->parent->$relationName) {
+            $value = $relation->pluck($this->localKey)->all();
+        }
+
+        return $value;
     }
 }
