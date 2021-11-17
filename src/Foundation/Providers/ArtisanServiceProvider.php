5507979f030e40855373dfc77bcf0a82ae14d5a1
Samuel Georges <sam@daftspunk.com>
2021-11-18 09:20:05 +1100

    Keep migrate commands as prep for 3.0

--- src/Foundation/Providers/ArtisanServiceProvider.php
+++ src/Foundation/Providers/ArtisanServiceProvider.php
@@ -14,32 +14,32 @@ class ArtisanServiceProvider extends ArtisanServiceProviderBase
      * @var array
      */
     protected $commands = [
-        'CacheClear'      => 'command.cache.clear',
-        'CacheForget'     => 'command.cache.forget',
-        'ClearCompiled'   => 'command.clear-compiled',
-        'ConfigCache'     => 'command.config.cache',
-        'ConfigClear'     => 'command.config.clear',
-        'Down'            => 'command.down',
-        'Environment'     => 'command.environment',
-        'KeyGenerate'     => 'command.key.generate',
-        'Optimize'        => 'command.optimize',
+        'CacheClear' => 'command.cache.clear',
+        'CacheForget' => 'command.cache.forget',
+        'ClearCompiled' => 'command.clear-compiled',
+        'ConfigCache' => 'command.config.cache',
+        'ConfigClear' => 'command.config.clear',
+        'Down' => 'command.down',
+        'Environment' => 'command.environment',
+        'KeyGenerate' => 'command.key.generate',
+        'Optimize' => 'command.optimize',
         'PackageDiscover' => 'command.package.discover',
-        'QueueFailed'     => 'command.queue.failed',
-        'QueueFlush'      => 'command.queue.flush',
-        'QueueForget'     => 'command.queue.forget',
-        'QueueListen'     => 'command.queue.listen',
-        'QueueRestart'    => 'command.queue.restart',
-        'QueueRetry'      => 'command.queue.retry',
-        'QueueWork'       => 'command.queue.work',
-        'RouteCache'      => 'command.route.cache',
-        'RouteClear'      => 'command.route.clear',
-        'RouteList'       => 'command.route.list',
-        'ScheduleFinish'  => \Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
-        'ScheduleRun'     => \Illuminate\Console\Scheduling\ScheduleRunCommand::class,
-        'Seed'            => 'command.seed',
-        'StorageLink'     => 'command.storage.link',
-        'Up'              => 'command.up',
-        'ViewClear'       => 'command.view.clear',
+        'QueueFailed' => 'command.queue.failed',
+        'QueueFlush' => 'command.queue.flush',
+        'QueueForget' => 'command.queue.forget',
+        'QueueListen' => 'command.queue.listen',
+        'QueueRestart' => 'command.queue.restart',
+        'QueueRetry' => 'command.queue.retry',
+        'QueueWork' => 'command.queue.work',
+        'RouteCache' => 'command.route.cache',
+        'RouteClear' => 'command.route.clear',
+        'RouteList' => 'command.route.list',
+        'ScheduleFinish' => \Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
+        'ScheduleRun' => \Illuminate\Console\Scheduling\ScheduleRunCommand::class,
+        'Seed' => 'command.seed',
+        'StorageLink' => 'command.storage.link',
+        'Up' => 'command.up',
+        'ViewClear' => 'command.view.clear',
     ];
 
     /**
@@ -48,8 +48,8 @@ class ArtisanServiceProvider extends ArtisanServiceProviderBase
      * @var array
      */
     protected $devCommands = [
-        'Serve'             => 'command.serve',
-        'VendorPublish'     => 'command.vendor.publish',
+        'Serve' => 'command.serve',
+        'VendorPublish' => 'command.vendor.publish',
     ];
 
     /**
