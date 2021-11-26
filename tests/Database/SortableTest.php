7b7be79201061559d7139411d3880c0a1f722edd
Samuel Georges <sam@daftspunk.com>
2021-11-26 14:20:17 +1100

    Fixes test

--- tests/Database/SortableTest.php
+++ tests/Database/SortableTest.php
@@ -16,15 +16,15 @@ class SortableTest extends TestCase
 
     public function testOrderByIsAutomaticallyAdded()
     {
-        $model = new TestModel();
+        $model = new TestModel;
         $query = $model->newQuery()->toSql();
 
-        $this->assertEquals('select * from "test" order by "sort_order" asc', $query);
+        $this->assertEquals('select * from "test" order by "test"."sort_order" asc', $query);
     }
 
     public function testOrderByCanBeOverridden()
     {
-        $model = new TestModel();
+        $model = new TestModel;
         $query1 = $model->newQuery()->orderBy('name')->orderBy('email', 'desc')->toSql();
         $query2 = $model->newQuery()->orderBy('sort_order')->orderBy('name')->toSql();
 
