1ab5183d50b14f17a861315dcb01de5b49e903db
Samuel Georges <sam@daftspunk.com>
2022-01-28 19:25:28 +1100

    Fixes test

--- tests/Translation/TranslatorTest.php
+++ tests/Translation/TranslatorTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Filesystem\Filesystem;
-use October\Rain\Events\Dispatcher;
 use October\Rain\Translation\FileLoader;
 use October\Rain\Translation\Translator;
 
@@ -10,13 +9,13 @@ class TranslatorTest extends TestCase
     /**
      * @var Translator
      */
-    private $translator;
+    protected $translator;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $path       = __DIR__ . '/../fixtures/lang';
+        $path = __DIR__ . '/../fixtures/lang';
         $fileLoader = new FileLoader(new Filesystem(), $path);
         $translator = new Translator($fileLoader, 'en');
         $this->translator = $translator;
@@ -30,16 +29,10 @@ class TranslatorTest extends TestCase
         );
     }
 
-    public function testOverrideWithBeforeResolveEvent()
+    public function testOverrideWithSet()
     {
-        $eventsDispatcher = $this->createMock(Dispatcher::class);
-        $eventsDispatcher
-            ->expects($this->exactly(2))
-            ->method('dispatch')
-            ->will($this->onConsecutiveCalls('Hello Override!', null));
-        $this->translator->setEventDispatcher($eventsDispatcher);
-
+        $this->assertEquals('lang.test.hello_override', $this->translator->get('lang.test.hello_override'));
+        $this->translator->set('lang.test.hello_override', 'Hello Override!');
         $this->assertEquals('Hello Override!', $this->translator->get('lang.test.hello_override'));
-        $this->assertEquals('Hello October!', $this->translator->get('lang.test.hello_october'));
     }
 }
