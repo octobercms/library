531620a4924895a0ae736eaa832a582e94cda39d
Sam Georges <sam@daftspunk.com>
2022-02-06 15:48:27 +1100

    Clean up migration scripts

---
+++ src/Halcyon/Migrations/2021_10_01_000001_Db_Templates.php
@@ -0,0 +1,25 @@
+<?php
+
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class DbTemplates extends Migration
+{
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('source')->index();
+            $table->string('path')->index();
+            $table->longText('content');
+            $table->integer('file_size')->unsigned();
+            $table->dateTime('updated_at')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+}
