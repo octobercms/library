c2c90eebffbfbd14ea8129b312943c7a2d990357
Sam Georges <sam@daftspunk.com>
2022-02-09 11:02:22 +1100

    Twig contracts

---
+++ contracts/Twig/CallsAnyMethod.php
@@ -0,0 +1,11 @@
+<?php namespace October\Contracts\Twig;
+
+/**
+ * CallsAnyMethod from Twig engine
+ *
+ * @package october\contracts
+ * @author Alexey Bobkov, Samuel Georges
+ */
+interface CallsAnyMethod
+{
+}
