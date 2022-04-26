df55be9c1f2bd06e391ad5c76b07dd0ec0dceb43
Sam Georges <sam@daftspunk.com>
2022-04-26 12:40:51 +1000

    Define element holder

---
+++ src/Element/ElementHolder.php
@@ -0,0 +1,12 @@
+<?php namespace October\Rain\Element;
+
+/**
+ * ElementHolder is a general collection used when passing a group of elements by reference,
+ * allowing access via array and object notation.
+ *
+ * @package october\element
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class ElementHolder extends ElementBase
+{
+}
