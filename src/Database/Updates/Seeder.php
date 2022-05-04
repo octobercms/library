1ef757a4aa52dd4c03384eebfa773c0900f6612e
Sam Georges <sam@daftspunk.com>
2022-05-04 14:08:13 +1000

    Adds line helper

--- src/Database/Updates/Seeder.php
+++ src/Database/Updates/Seeder.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Database\Seeder as SeederBase;
 
+/**
+ * Seeder
+ */
 class Seeder extends SeederBase
 {
+    /**
+     * line writes a string as standard output.
+     * @param  string  $string
+     * @param  string|null  $style
+     * @return void
+     */
+    public function line($string, $style = null)
+    {
+        if (!isset($this->command)) {
+            return;
+        }
+
+        $styled = $style ? "<$style>$string</$style>" : $string;
+
+        $this->command->getOutput()->writeln($styled);
+    }
 }
