8c88f89b898c91b0f444786a79b1791f05cae76b
Sam Georges <realsamgeorges@gmail.com>
2022-05-19 10:17:15 +1000

    Fixes iteration on holder

--- src/Element/ElementHolder.php
+++ src/Element/ElementHolder.php
@@ -1,5 +1,9 @@
 <?php namespace October\Rain\Element;
 
+use October\Rain\Support\Collection;
+use IteratorAggregate;
+use Traversable;
+
 /**
  * ElementHolder is a general collection used when passing a group of elements by reference,
  * allowing access via array and object notation.
@@ -7,6 +11,13 @@
  * @package october\element
  * @author Alexey Bobkov, Samuel Georges
  */
-class ElementHolder extends ElementBase
+class ElementHolder extends ElementBase implements IteratorAggregate
 {
+    /**
+     * getIterator for the elements.
+     */
+    public function getIterator(): Traversable
+    {
+        return new Collection($this->config);
+    }
 }
