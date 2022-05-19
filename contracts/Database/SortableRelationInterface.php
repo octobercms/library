59f3b1d79c5387d251315adac72f19626da3868b
Sam Georges <realsamgeorges@gmail.com>
2022-05-19 11:10:02 +1000

    Fixes issue with dual sort
    When a model implements two types of sorting (eg: belongsToMany+hasMany), the sortable relation interface is not enough to check if the setSortableRelationOrder method should be called. An isSortableRelation method can be used in conjunction with the interface check. The setSortableRelationOrder method also throws a more meaningful error.

--- contracts/Database/SortableRelationInterface.php
+++ contracts/Database/SortableRelationInterface.php
@@ -9,7 +9,16 @@
 interface SortableRelationInterface
 {
     /**
-     * setSortableRelationOrder
+     * setSortableRelationOrder sets the sort order of records to the specified orders. If the orders is
+     * undefined, the record identifier is used.
+     * @param string $relationName
+     * @param mixed $itemIds
+     * @param array $itemOrders
      */
     public function setSortableRelationOrder($relationName, $itemIds, $itemOrders = null);
+
+    /**
+     * isSortableRelation returns true if the supplied relation is sortable.
+     */
+    public function isSortableRelation($relationName);
 }
