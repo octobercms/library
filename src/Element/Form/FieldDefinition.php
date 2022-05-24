61244ddc52a639f0ac941e06a4dc548261de7b8e
Sam Georges <realsamgeorges@gmail.com>
2022-05-24 19:13:28 +1000

    hasOptions returns true if options() is callable

--- src/Element/Form/FieldDefinition.php
+++ src/Element/Form/FieldDefinition.php
@@ -100,7 +100,8 @@ class FieldDefinition extends ElementBase
      */
     public function hasOptions(): bool
     {
-        return $this->options !== null;
+        return $this->options !== null &&
+            (is_array($this->options) || is_callable($this->options));
     }
 
     /**
