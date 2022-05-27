681dc413000e2dc286f9db5c00ac7a5f8280e303
Sam Georges <realsamgeorges@gmail.com>
2022-05-27 13:53:34 +1000

    Default aliases

--- src/Support/Facade.php
+++ src/Support/Facade.php
@@ -36,4 +36,22 @@ class Facade extends FacadeParent
     {
         return null;
     }
+
+    /**
+     * defaultAliases gets the application default aliases.
+     * @return \Illuminate\Support\Collection
+     */
+    public static function defaultAliases()
+    {
+        return parent::defaultAliases()->merge([
+            'Model' => \October\Rain\Database\Model::class,
+            'Event' => \October\Rain\Support\Facades\Event::class,
+            'Mail' => \October\Rain\Support\Facades\Mail::class,
+            'File' => \October\Rain\Support\Facades\File::class,
+            'Config' => \October\Rain\Support\Facades\Config::class,
+            'Seeder' => \October\Rain\Database\Updates\Seeder::class,
+            'Input' => \October\Rain\Support\Facades\Input::class,
+            'Str' => \October\Rain\Support\Facades\Str::class,
+        ]);
+    }
 }
