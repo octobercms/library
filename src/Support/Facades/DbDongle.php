83d3b30eebc28d653327b1358ecc6a5c118f762c
Jacob de Keizer <jacob@vdlp.nl>
2022-06-02 22:09:18 +1000

    Readd facade docblocks

--- src/Support/Facades/DbDongle.php
+++ src/Support/Facades/DbDongle.php
@@ -5,6 +5,18 @@ use October\Rain\Support\Facade;
 /**
  * DbDongle
  *
+ * @method static mixed raw(string $sql)
+ * @method static string parse(string $sql)
+ * @method static string parseGroupConcat(string $sql)
+ * @method static string parseConcat(string $sql)
+ * @method static string parseIfNull(string $sql)
+ * @method static string parseBooleanExpression(string $sql)
+ * @method static string cast(string $sql, string $asType = 'INTEGER')
+ * @method static void convertTimestamps(string $table, string|array $columns = null)
+ * @method static void disableStrictMode()
+ * @method static string getDriver()
+ * @method static string getTablePrefix()
+ *
  * @see \October\Rain\Database\Dongle
  */
 class DbDongle extends Facade
--- src/Support/Facades/Ini.php
+++ src/Support/Facades/Ini.php
@@ -5,6 +5,11 @@ use October\Rain\Support\Facade;
 /**
  * Ini
  *
+ * @method static array parse(string $contents)
+ * @method static array parseFile(string $fileName)
+ * @method static array expandProperty(array &$array, string $key, $value)
+ * @method static string render(array $vars = [], int $level = 1)
+ *
  * @see \October\Rain\Parse\Ini
  */
 class Ini extends Facade
