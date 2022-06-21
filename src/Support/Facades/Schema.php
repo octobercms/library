bb9e801f99a9e7797c67d0519bd6d9dbdc429e46
Sam Georges <realsamgeorges@gmail.com>
2022-06-21 13:56:38 +1000

    Restore Schema facade

---
+++ src/Support/Facades/Schema.php
@@ -0,0 +1,21 @@
+<?php namespace October\Rain\Support\Facades;
+
+use Illuminate\Support\Facades\Schema as SchemaBase;
+
+/**
+ * Schema
+ *
+ * @deprecated use \Schema
+ * @see \Illuminate\Database\Schema\Builder
+ */
+class Schema extends SchemaBase
+{
+    /**
+     * getFacadeAccessor returns the registered name of the component
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'db.schema';
+    }
+}
