74858b5d3b381d92b5f76825611aacdbe1613e5a
October CMS <hello@octobercms.com>
2023-01-21 12:08:34 +1100

    Restore attributes after save, set default priority

--- src/Database/ExpandoModel.php
+++ src/Database/ExpandoModel.php
@@ -20,6 +20,12 @@ class ExpandoModel extends Model
     protected $expandoPassthru = [];
 
     /**
+     * @var int expandoPriority events should come first but make room for others,
+     * assuming a range of 1 to 1000.
+     */
+    protected $expandoPriority = 300;
+
+    /**
      * __construct
      */
     public function __construct(array $attributes = [])
@@ -28,9 +34,11 @@ class ExpandoModel extends Model
 
         $this->addJsonable($this->expandoColumn);
 
-        $this->bindEvent('model.afterFetch', [$this, 'expandoAfterFetch']);
+        $this->bindEvent('model.afterFetch', [$this, 'expandoAfterFetch'], $this->expandoPriority);
 
-        $this->bindEvent('model.saveInternal', [$this, 'expandoSaveInternal']);
+        $this->bindEvent('model.saveInternal', [$this, 'expandoSaveInternal'], $this->expandoPriority);
+
+        $this->bindEvent('model.afterSave', [$this, 'expandoAfterSave'], $this->expandoPriority);
     }
 
     /**
@@ -54,6 +62,14 @@ class ExpandoModel extends Model
         );
 
         $this->attributes = array_diff_key($this->attributes, $this->{$this->expandoColumn});
+    }
+
+    /**
+     * expandoAfterSave
+     */
+    public function expandoAfterSave()
+    {
+        $this->attributes = array_merge($this->{$this->expandoColumn}, $this->attributes);
     }
 
     /**
