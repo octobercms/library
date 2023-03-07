10d9c0cbd15777f13245a437a577b1bcc7cff79d
Nick Khaetsky <rzzsapb@gmail.com>
2023-03-07 14:18:51 +0300

    Add null to PHPDOC

--- src/Database/Attach/File.php
+++ src/Database/Attach/File.php
@@ -230,7 +230,7 @@ class File extends Model
 
     /**
      * getWidthAttribute helper attribute for get image width
-     * @return string
+     * @return string|bool
      */
     public function getWidthAttribute()
     {
@@ -242,7 +242,7 @@ class File extends Model
 
     /**
      * getHeightAttribute helper attribute for get image height
-     * @return string
+     * @return string|bool
      */
     public function getHeightAttribute()
     {
