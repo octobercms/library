638625b9bea763abc204fed7c43850b8619e53fd
October CMS <hello@octobercms.com>
2023-06-07 10:53:32 +1000

    Target plugin seeders

--- src/Scaffold/Console/CreateSeeder.php
+++ src/Scaffold/Console/CreateSeeder.php
@@ -31,10 +31,10 @@ class CreateSeeder extends GeneratorCommandBase
     public function makeStubs()
     {
         if ($this->isAppNamespace()) {
-            $this->makeStub('seeder/seeder.stub', 'database/seeds/{{studly_name}}.php');
+            $this->makeStub('seeder/create_app_seeder.stub', 'database/seeds/{{studly_name}}.php');
         }
         else {
-            $this->makeStub('seeder/seeder.stub', 'updates/{{studly_name}}.php');
+            $this->makeStub('seeder/create_seeder.stub', 'updates/{{snake_name}}.php');
         }
     }
 
