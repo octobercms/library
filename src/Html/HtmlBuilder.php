d726e3a3180990efdb33360d02a4572ae4ac7f15
October CMS <hello@octobercms.com>
2023-06-22 16:01:17 +1000

    True casts to anonymous attribute

--- src/Html/HtmlBuilder.php
+++ src/Html/HtmlBuilder.php
@@ -349,7 +349,10 @@ class HtmlBuilder
             return;
         }
 
-        if (is_array($value)) {
+        if ($value === true) {
+            return $key;
+        }
+        elseif (is_array($value)) {
             $value = substr(htmlspecialchars(json_encode($value), ENT_QUOTES, 'UTF-8'), 1, -1);
         }
         else {
