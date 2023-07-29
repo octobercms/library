55722a5db40908b65ab477283425d57af0236eaf
October CMS <hello@octobercms.com>
2023-07-29 15:54:23 +1000

    Adds index/position support to error messages

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -4,7 +4,9 @@ use Illuminate\Validation\Validator as ValidatorBase;
 use October\Rain\Exception\ValidationException;
 
 /**
- * Validator is a modifier to the base class
+ * Validator is a modifier to the base class, it extends validation rules
+ * with extra methods for specifying messages and replacements inside the
+ * class definition.
  */
 class Validator extends ValidatorBase
 {
