4d377604163c02564d965c82e644f99042b4a6a3
October CMS <hello@octobercms.com>
2023-09-29 17:12:52 +1000

    Deprecate

--- src/Support/Facades/Site.php
+++ src/Support/Facades/Site.php
@@ -29,8 +29,6 @@ class Site extends Facade
      */
     protected static function getFacadeAccessor()
     {
-        // @deprecated use below
-        // return 'system.sites';
-        return 'site.manager';
+        return 'system.sites';
     }
 }
