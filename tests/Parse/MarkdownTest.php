387541e5f7921c4ba188597a9dc8109392fe75f5
Samuel Georges <sam@daftspunk.com>
2024-01-16 08:22:46 +1100

    Adds more markdown tests

--- tests/Parse/MarkdownTest.php
+++ tests/Parse/MarkdownTest.php
@@ -1,12 +1,21 @@
 <?php
 
 use October\Rain\Parse\Markdown;
+use October\Rain\Events\FakeDispatcher;
+use October\Rain\Events\Dispatcher;
 
 /**
  * MarkdownTest
  */
 class MarkdownTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Event::swap(new FakeDispatcher(new Dispatcher));
+    }
+
     /**
      * testParseIndent
      */
@@ -66,5 +75,80 @@ HTML;
         $text = '<div><p>Foo</p><p>Bar</p></div>';
         $normal = $parser->parse($text);
         $this->assertEquals("<div><p>Foo</p><p>Bar</p></div>", $normal);
+    }
+
+    public function testParseNonHtml()
+    {
+        $parser = new Markdown;
+
+        $text = <<<TEXT
+<table
+
+some other text
+
+## hello
+
+TEXT;
+
+$expected = '<p>&lt;table</p>
+<p>some other text</p>
+<h2>hello</h2>';
+
+        $normal = $parser->parse($text);
+
+        // Only accepting one node per line
+        $this->assertEquals($expected, $normal);
+    }
+
+    public function testParseMultilineHtml()
+    {
+        $parser = new Markdown;
+
+        $text = <<<HTML
+<div>
+<table width="100%"
+       align="center"
+       border="0"
+       cellpadding="0"
+       cellspacing="0"
+       style="background: red; min-height: 500px;">
+    <thead>
+    <tr>
+        <th>Test</th>
+        <th>123</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Lorem</td>
+        <td>Ipsum</td>
+    </tr>
+    </tbody>
+</table>
+</div>
+HTML;
+
+        $expected = <<<HTML
+<div>
+<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="background: red; min-height: 500px;">
+    <thead>
+    <tr>
+        <th>Test</th>
+        <th>123</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Lorem</td>
+        <td>Ipsum</td>
+    </tr>
+    </tbody>
+</table>
+</div>
+HTML;
+
+        $normal = $parser->parse($text);
+
+        $this->assertEquals($expected, $normal);
     }
 }
