14ea820a87d714cb44f227bb0c1942416a802976
Samuel Georges <sam@daftspunk.com>
2025-06-01 14:43:06 +1000

    Fixes base path target

--- init/autoloader.php
+++ init/autoloader.php
@@ -2,7 +2,7 @@
 
 use October\Rain\Composer\ClassLoader;
 
-ClassLoader::configure(dirname(__DIR__))
+ClassLoader::configure(dirname(__DIR__, 4))
     ->withNamespace('App\\', '')
     ->withDirectories([
         'modules',
