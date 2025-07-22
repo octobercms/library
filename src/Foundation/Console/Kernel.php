fdbdb4967ede5234802b431911eddd1ec40bd4eb
vdlp-mw <47185594+vdlp-mw@users.noreply.github.com>
2025-07-22 16:16:17 +0200

    Use October\...\HandleExceptions in Console\Kernel
    Implements October HandleExceptions class in the console kernel. This ensures that the new `system.log_deprecations` config option is checked before logging deprecation.
    
    This change is already implemented in the Http\Kernel which was added in https://github.com/octobercms/library/commit/ed6aa00a590f232fac38fd5bb8406f1ca9149afe

--- src/Foundation/Console/Kernel.php
+++ src/Foundation/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected $bootstrappers = [
         \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
         \October\Rain\Foundation\Bootstrap\LoadConfiguration::class,
-        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
+        \October\Rain\Foundation\Bootstrap\HandleExceptions::class,
         \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
         \Illuminate\Foundation\Bootstrap\SetRequestForConsole::class,
         \October\Rain\Foundation\Bootstrap\RegisterOctober::class,
