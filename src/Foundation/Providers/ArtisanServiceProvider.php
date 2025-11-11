db10e7e59af1116fe3c7a82f460de9aba2713d2c
Samuel Georges <sam@daftspunk.com>
2025-11-11 19:48:35 +1100

    Inherit all laravel-based artisan commands

--- src/Foundation/Providers/ArtisanServiceProvider.php
+++ src/Foundation/Providers/ArtisanServiceProvider.php
@@ -1,49 +1,11 @@
 <?php namespace October\Rain\Foundation\Providers;
 
+use Illuminate\Console\Signals;
 use October\Rain\Foundation\Console\ServeCommand;
 use October\Rain\Foundation\Console\RouteListCommand;
 use October\Rain\Foundation\Console\RouteCacheCommand;
 use October\Rain\Foundation\Console\ProjectSetCommand;
 use October\Rain\Foundation\Console\ClearCompiledCommand;
-use Illuminate\Cache\Console\ClearCommand as CacheClearCommand;
-use Illuminate\Cache\Console\ForgetCommand as CacheForgetCommand;
-use Illuminate\Database\Console\Seeds\SeedCommand;
-use Illuminate\Database\Console\WipeCommand;
-use Illuminate\Foundation\Console\ConfigCacheCommand;
-use Illuminate\Foundation\Console\ConfigClearCommand;
-use Illuminate\Foundation\Console\DownCommand;
-use Illuminate\Foundation\Console\EnvironmentCommand;
-use Illuminate\Foundation\Console\EventCacheCommand;
-use Illuminate\Foundation\Console\EventClearCommand;
-use Illuminate\Foundation\Console\EventListCommand;
-use Illuminate\Foundation\Console\KeyGenerateCommand;
-use Illuminate\Foundation\Console\OptimizeClearCommand;
-use Illuminate\Foundation\Console\OptimizeCommand;
-use Illuminate\Foundation\Console\PackageDiscoverCommand;
-use Illuminate\Foundation\Console\RouteClearCommand;
-use Illuminate\Foundation\Console\StorageLinkCommand;
-use Illuminate\Console\Scheduling\ScheduleClearCacheCommand;
-use Illuminate\Console\Scheduling\ScheduleFinishCommand;
-use Illuminate\Console\Scheduling\ScheduleListCommand;
-use Illuminate\Console\Scheduling\ScheduleRunCommand;
-use Illuminate\Console\Scheduling\ScheduleTestCommand;
-use Illuminate\Console\Scheduling\ScheduleWorkCommand;
-use Illuminate\Foundation\Console\UpCommand;
-use Illuminate\Foundation\Console\VendorPublishCommand;
-use Illuminate\Foundation\Console\ViewCacheCommand;
-use Illuminate\Foundation\Console\ViewClearCommand;
-use Illuminate\Queue\Console\ClearCommand as QueueClearCommand;
-use Illuminate\Queue\Console\FlushFailedCommand as FlushFailedQueueCommand;
-use Illuminate\Queue\Console\ForgetFailedCommand as ForgetFailedQueueCommand;
-use Illuminate\Queue\Console\ListenCommand as QueueListenCommand;
-use Illuminate\Queue\Console\ListFailedCommand as ListFailedQueueCommand;
-use Illuminate\Queue\Console\MonitorCommand as QueueMonitorCommand;
-use Illuminate\Queue\Console\PruneBatchesCommand as QueuePruneBatchesCommand;
-use Illuminate\Queue\Console\PruneFailedJobsCommand as QueuePruneFailedJobsCommand;
-use Illuminate\Queue\Console\RestartCommand as QueueRestartCommand;
-use Illuminate\Queue\Console\RetryBatchCommand as QueueRetryBatchCommand;
-use Illuminate\Queue\Console\RetryCommand as QueueRetryCommand;
-use Illuminate\Queue\Console\WorkCommand as QueueWorkCommand;
 use Illuminate\Foundation\Providers\ArtisanServiceProvider as ArtisanServiceProviderBase;
 
 /**
@@ -56,62 +18,18 @@ class ArtisanServiceProvider extends ArtisanServiceProviderBase
      *
      * @var array
      */
-    protected $commands = [
-        // Laravel
-        //
-        'CacheClear' => CacheClearCommand::class,
-        'CacheForget' => CacheForgetCommand::class,
-        'ClearCompiled' => ClearCompiledCommand::class,
-        'ConfigCache' => ConfigCacheCommand::class,
-        'ConfigClear' => ConfigClearCommand::class,
-        'DbWipe' => WipeCommand::class,
-        'Down' => DownCommand::class,
-        'Environment' => EnvironmentCommand::class,
-        'EventCache' => EventCacheCommand::class,
-        'EventClear' => EventClearCommand::class,
-        'EventList' => EventListCommand::class,
-        'KeyGenerate' => KeyGenerateCommand::class,
-        'Optimize' => OptimizeCommand::class,
-        'OptimizeClear' => OptimizeClearCommand::class,
-        'PackageDiscover' => PackageDiscoverCommand::class,
-        'QueueClear' => QueueClearCommand::class,
-        'QueueFailed' => ListFailedQueueCommand::class,
-        'QueueFlush' => FlushFailedQueueCommand::class,
-        'QueueForget' => ForgetFailedQueueCommand::class,
-        'QueueListen' => QueueListenCommand::class,
-        'QueueMonitor' => QueueMonitorCommand::class,
-        'QueuePruneBatches' => QueuePruneBatchesCommand::class,
-        'QueuePruneFailedJobs' => QueuePruneFailedJobsCommand::class,
-        'QueueRestart' => QueueRestartCommand::class,
-        'QueueRetry' => QueueRetryCommand::class,
-        'QueueRetryBatch' => QueueRetryBatchCommand::class,
-        'QueueWork' => QueueWorkCommand::class,
-        'RouteCache' => RouteCacheCommand::class,
-        'RouteClear' => RouteClearCommand::class,
+    protected $commandsRain = [
         'RouteList' => RouteListCommand::class,
-        'ScheduleFinish' => ScheduleFinishCommand::class,
-        'ScheduleList' => ScheduleListCommand::class,
-        'ScheduleRun' => ScheduleRunCommand::class,
-        'ScheduleClearCache' => ScheduleClearCacheCommand::class,
-        'ScheduleTest' => ScheduleTestCommand::class,
-        'ScheduleWork' => ScheduleWorkCommand::class,
-        'Seed' => SeedCommand::class,
-        'StorageLink' => StorageLinkCommand::class,
-        'Up' => UpCommand::class,
-        'ViewCache' => ViewCacheCommand::class,
-        'ViewClear' => ViewClearCommand::class,
-
-        // October
-        //
+        'RouteCache' => RouteCacheCommand::class,
         'ProjectSet' => ProjectSetCommand::class,
+        'ClearCompiled' => ClearCompiledCommand::class,
     ];
 
     /**
      * @var array devCommands to be registered.
      */
-    protected $devCommands = [
+    protected $devCommandsRain = [
         'Serve' => ServeCommand::class,
-        'VendorPublish' => VendorPublishCommand::class,
     ];
 
     /**
@@ -119,7 +37,18 @@ class ArtisanServiceProvider extends ArtisanServiceProviderBase
      */
     public function register()
     {
-        parent::register();
+        $this->registerCommands(array_merge(
+            $this->commands,
+            $this->commandsRain,
+            $this->devCommands,
+            $this->devCommandsRain
+        ));
+
+        Signals::resolveAvailabilityUsing(function () {
+            return $this->app->runningInConsole()
+                && ! $this->app->runningUnitTests()
+                && extension_loaded('pcntl');
+        });
     }
 
     /**
