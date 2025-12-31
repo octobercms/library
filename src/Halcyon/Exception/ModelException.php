a9f4317bd1b5bd65d59689fa72daf1a84786fc92
Samuel <Samuell1@users.noreply.github.com>
2025-12-31 14:04:16 +0100

    Update ModelException.php

--- src/Halcyon/Exception/ModelException.php
+++ src/Halcyon/Exception/ModelException.php
@@ -2,6 +2,8 @@
 
 use October\Rain\Halcyon\Model;
 use October\Rain\Exception\ValidationException;
+use Illuminate\Support\MessageBag;
+use Exception;
 
 /**
  * ModelException used when validation fails, contains the invalid model for easy analysis
@@ -17,13 +19,38 @@ class ModelException extends ValidationException
     protected $model;
 
     /**
+     * @var MessageBag validationErrors
+     */
+    protected $validationErrors;
+
+    /**
      * __construct receives the invalid model
      */
     public function __construct(Model $model)
     {
         $this->model = $model;
-        $this->errors = $model->errors();
+        $this->validationErrors = $model->errors();
+
+        // Bypass parent constructor to avoid Validator facade dependency
+        Exception::__construct($this->validationErrors->first());
+
         $this->evalErrors();
+    }
+
+    /**
+     * errors returns validation errors
+     */
+    public function errors(): array
+    {
+        return $this->validationErrors->messages();
+    }
+
+    /**
+     * getErrors returns the message bag instance
+     */
+    public function getErrors(): MessageBag
+    {
+        return $this->validationErrors;
     }
 
     /**
